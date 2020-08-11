da144cef3d5483f3fa8d7881aa7092bf2727a9d1
Sam Chen <sam.chen@iota.org>
2020-08-11 20:26:17 +0800

    Added as Platformio library.

---
+++ src/network/http_esp32.c
@@ -0,0 +1,14 @@
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "http.h"
+
+void http_client_init() {}
+
+void http_client_clean() {}
+
+void http_client_post(http_buf_t* const response, char const* const url, http_buf_t const* const request) {}
+
+void http_client_get(http_buf_t* const response, char const* const url) {}
