3b7edafcfd865d9bd2b9688df1fd755ea1440caf
Sam Chen <sam.chen@iota.org>
2020-09-16 16:45:21 +0800

    Update http test cases.

--- tests/test_http.c
+++ tests/test_http.c
@@ -13,10 +13,11 @@ void test_http() {
   http_client_config_t conf = {0};
   http_buf_t* response = http_buf_new();
   http_buf_t* req = http_buf_new_with_data((byte_t*)str, strlen(str) + 1);
-  conf.url = "https://postman-echo.com/post";
+  conf.url = "https://httpbin.org/post";
   http_client_post(response, &conf, req);
-  http_buf2str(response);  // convert data to string for printf debugging.
-  printf("%s\n", response->data);
+  TEST_ASSERT_NOT_NULL(response->data);
+  // http_buf2str(response);  // convert data to string for printf debugging.
+  // printf("%s\n", response->data);
   http_buf_free(response);
   http_buf_free(req);
   req = NULL;
@@ -24,24 +25,53 @@ void test_http() {
 
   //=========HTTPS GET==========
   response = http_buf_new();
-  conf.url = "https://example.com/";
+  conf.url = "https://httpbin.org/get";
   http_client_get(response, &conf);
-  http_buf2str(response);  // convert data to string for printf debugging.
+  TEST_ASSERT_NOT_NULL(response->data);
+  // http_buf2str(response);  // convert data to string for printf debugging.
+  // printf("%s\n", response->data);
+  http_buf_free(response);
+  response = NULL;
+
+  //=========HTTPS Stream==========
+  response = http_buf_new();
+  conf.url = "https://httpbin.org/stream-bytes/101";
+  http_client_get(response, &conf);
+  TEST_ASSERT_NOT_NULL(response->data);
+  TEST_ASSERT_EQUAL_UINT32(101, response->len);
+  // printf("%zu\n", response->len);
+  http_buf_free(response);
+  response = NULL;
+
+  //=========HTTPS base64 decode==========
+  response = http_buf_new();
+  conf.url = "https://httpbin.org/base64/SFRUUEJJTiBpcyBhd2Vzb21l";
+  http_client_get(response, &conf);
+  TEST_ASSERT_NOT_NULL(response->data);
+  http_buf2str(response);  // convert data to string for strcmp.
+  TEST_ASSERT_EQUAL_STRING("HTTPBIN is awesome", response->data);
+  // printf("%s\n", response->data);
+  http_buf_free(response);
+  response = NULL;
+
+  //=========HTTPS delay==========
+  response = http_buf_new();
+  conf.url = "https://httpbin.org/delay/5";
+  http_client_get(response, &conf);
+  TEST_ASSERT_NOT_NULL(response->data);
+  http_buf2str(response);
   printf("%s\n", response->data);
   http_buf_free(response);
   response = NULL;
 
-  http_client_clean();
-}
-
-void test_restful() {
-  http_client_init();
-  http_client_config_t conf = {0};
-  http_buf_t* response = http_buf_new();
-  conf.url = "https://virtserver.swaggerhub.com/oopsmonk/mytest/0.0.1/messages/by-hash?hashes=thisisatesthash";
+  //=========HTTPS Stream JSON==========
+  response = http_buf_new();
+  conf.url = "https://httpbin.org/stream/3";
   http_client_get(response, &conf);
-  // printf("%.*s\n", response->len, response->data);
+  TEST_ASSERT_NOT_NULL(response->data);
+  printf("data size: %zu\n", response->len);
   http_buf2str(response);  // convert data to string for printf debugging.
+  printf("string size: %zu\n", response->len);
   printf("%s\n", response->data);
   http_buf_free(response);
   response = NULL;
@@ -53,7 +83,6 @@ int main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_http);
-  RUN_TEST(test_restful);
 
   return UNITY_END();
 }
