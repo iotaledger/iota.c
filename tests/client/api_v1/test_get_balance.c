6356d233fe21d19111207e756d5f75dce9d8b558
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-11-26 00:17:09 +0100

    elaborate balance test boilerplate

--- tests/client/api_v1/test_get_balance.c
+++ tests/client/api_v1/test_get_balance.c
@@ -3,18 +3,32 @@
 #include <string.h>
 #include <unity/unity.h>
 
+#include "core/utils/byte_buffer.h"
 #include "client/api/v1/get_balance.h"
 
-#define ADDR "7ed3d67fc7b619e72e588f51fef2379e43e6e9a856635843b3f29aa3a3f1f006"
+#define ADDR_HEX "7ed3d67fc7b619e72e588f51fef2379e43e6e9a856635843b3f29aa3a3f1f006"
 
 void test_get_balance() {
     res_balance_t *res = calloc(1, sizeof(res_balance_t));
-    byte_t *addr = "";
+    byte_t addr[IOTA_ADDRESS_BYTES];
 
+    // test null cases
     TEST_ASSERT_EQUAL_INT(-1, get_balance(NULL, NULL));
     TEST_ASSERT_EQUAL_INT(-1, get_balance(NULL, res));
     TEST_ASSERT_EQUAL_INT(-1, get_balance("", NULL));
     TEST_ASSERT_EQUAL_INT(-1, get_balance("", res));
+
+    // test W-OTS
+    addr[IOTA_ADDRESS_BYTES-1] = 0;
+
+    // convert hex2bin
+    // TEST_ASSERT_EQUAL_INT(0, hex2bin(ADDR_HEX, addr, IOTA_ADDRESS_BYTES));
+
+    // test ed25519
+    addr[IOTA_ADDRESS_BYTES-1] = 1;
+
+    // test for success
+    // TEST_ASSERT_EQUAL_INT(0, get_balance(addr, res));
 }
 
 int main() {
