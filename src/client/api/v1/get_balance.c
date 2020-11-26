bad4381084952d049cc56d871c2822fdfddd4fd0
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-11-26 16:50:12 +0100

    add http client

--- src/client/api/v1/get_balance.c
+++ src/client/api/v1/get_balance.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "client/api/json_utils.h"
+#include "client/network/http.h"
 #include "core/utils/iota_str.h"
 #include "client/api/v1/get_balance.h"
 
@@ -21,6 +23,35 @@ int get_balance(iota_client_conf_t const *conf, byte_t addr[IOTA_ADDRESS_BYTES],
   }
 
   memcpy(res->addr, addr, strlen(addr)+1);
+
+  // http client configuration
+  http_client_config_t http_conf = {0};
+  http_conf.url = cmd->buf;
+  if (conf->port) {
+      http_conf.port = conf->port;
+  }
+
+  byte_buf_t *http_res = byte_buf_new();
+  if (http_res == NULL) {
+      printf("[%s:%d]: OOM\n", __func__, __LINE__);
+      // TODO
+      ret = -1;
+      goto done;
+  }
+
+  // send request via http client
+  http_client_get(&http_conf, http_res);
+  byte_buf2str(http_res);
+
+  // json deserialization
+  // deser_node_info((char const *const)http_res->data, res);
+
+  done:
+  // cleanup command
+  iota_str_destroy(cmd);
+  byte_buf_free(http_res);
+
+  return ret;
 
   // ...
 
