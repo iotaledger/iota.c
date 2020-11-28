7a951a718de7450ae12c0ecba430ff0550d469a3
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-11-28 19:03:51 +0100

    test deser_balance_info

--- tests/client/api_v1/test_get_balance.c
+++ tests/client/api_v1/test_get_balance.c
@@ -36,9 +36,37 @@ void test_get_balance() {
   TEST_ASSERT_EQUAL_INT(0, get_balance(&conf, addr, res));
 }
 
+void test_deser_balance_info() {
+  char const* json_info_200 =
+      "{\"data\":{\"address\": \"7ed3d67fc7b619e72e588f51fef2379e43e6e9a856635843b3f29aa3a3f1f006\","
+      "\"maxResults\": 1000,"
+      "\"count\": 25,"
+      "\"balance\": 1338263}}";
+
+  res_balance_t *res = calloc(1, sizeof(res_balance_t));
+//
+//  char const* json_info_400 =
+//      "{\"error\": {"
+//      "\"code\": \"invalid_data, "
+//      "\"message\": \"invalid data provided\"}}";
+//
+//  char const* json_info_404 =
+//      "{\"error\": {"
+//      "{\"code\": \"not_found\""
+//      "{\"message\": \"could not find data\"}}"
+
+  TEST_ASSERT_EQUAL_INT(0, deser_balance_info(json_info_200, res));
+  TEST_ASSERT_EQUAL_INT(1000, res->maxResults);
+  TEST_ASSERT_EQUAL_INT(25, res->count);
+  TEST_ASSERT_EQUAL_INT(1338263, res->balance);
+
+  free(res);
+}
+
 int main() {
   UNITY_BEGIN();
 
+  RUN_TEST(test_deser_balance_info);
   RUN_TEST(test_get_balance);
 
   return UNITY_END();
