ef5bc58e6351c7763704520ce922c8629c8eacc3
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-12-14 02:26:52 +0000

    api_v1 get_balance

---
+++ src/client/api/v1/get_balance.h
@@ -0,0 +1,67 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef __CLIENT_API_V1_BALANCE_H__
+#define __CLIENT_API_V1_BALANCE_H__
+
+#include <stdint.h>
+
+#include "client/api/v1/response_error.h"
+#include "client/client_service.h"
+#include "core/address.h"
+#include "core/types.h"
+
+typedef struct {
+  byte_t addr[IOTA_ADDRESS_HEX_BYTES];
+  uint16_t max_results;
+  uint16_t count;
+  int64_t balance;
+} get_balance_t;
+
+typedef struct {
+  bool is_error;
+  union {
+    res_err_t *error;
+    get_balance_t *output_balance;
+  } u;
+} res_balance_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+ * @brief Allocates balance response object
+ * @return res_balance_t*
+ */
+res_balance_t *res_balance_new();
+
+/**
+ * @brief Frees an balance response object
+ * @param[in] res A response object
+ */
+void res_balance_free(res_balance_t *res);
+
+/**
+ * @brief balance info JSON deserialization
+ *
+ * @param[in] j_str A string of json object
+ * @param[out] res A response object of balance info
+ * @return int 0 on success
+ */
+int deser_balance_info(char const *const j_str, res_balance_t *res);
+
+/**
+ * @brief Gets balance from address
+ *
+ * @param[in] ctx IOTA Client conf
+ * @param[in] addr The address
+ * @param[out] res A response object of balance info
+ * @return int 0 on success
+ */
+int get_balance(iota_client_conf_t const *ctx, char *addr, res_balance_t *res);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __CLIENT_API_V1_BALANCE_H__
---
+++ tests/client/api_v1/test_get_balance.c
@@ -0,0 +1,77 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unity/unity.h>
+
+#include "client/api/v1/get_balance.h"
+#include "core/utils/byte_buffer.h"
+
+#define ADDR_HEX "7ed3d67fc7b619e72e588f51fef2379e43e6e9a856635843b3f29aa3a3f1f006"
+
+void test_get_balance() {
+  iota_client_conf_t conf = {
+      .url = "http://0.0.0.0/",
+      .port = 14265  // use default port number
+  };
+
+  res_balance_t* res = calloc(1, sizeof(res_balance_t));
+
+  // test null cases
+  TEST_ASSERT_EQUAL_INT(-1, get_balance(NULL, NULL, NULL));
+  TEST_ASSERT_EQUAL_INT(-1, get_balance(NULL, NULL, res));
+  TEST_ASSERT_EQUAL_INT(-1, get_balance(&conf, NULL, NULL));
+  TEST_ASSERT_EQUAL_INT(-1, get_balance(&conf, NULL, res));
+
+  // test for success
+  TEST_ASSERT_EQUAL_INT(0, get_balance(&conf, ADDR_HEX, res));
+}
+
+void test_deser_balance_info() {
+  char const* json_info_200 =
+      "{\"data\":{\"address\": \"7ed3d67fc7b619e72e588f51fef2379e43e6e9a856635843b3f29aa3a3f1f006\","
+      "\"maxResults\": 1000,"
+      "\"count\": 25,"
+      "\"balance\": 1338263}}";
+
+  char const* json_info_400 =
+      "{\"error\": {"
+      "\"code\": \"invalid_data\", "
+      "\"message\": \"invalid data provided\"}}";
+
+  char const* json_info_404 =
+      "{\"error\": {"
+      "\"code\": \"not_found\", "
+      "\"message\": \"could not find data\"}}";
+
+  // test http status code 200
+  res_balance_t* res = res_balance_new();
+  TEST_ASSERT_EQUAL_INT(0, deser_balance_info(json_info_200, res));
+  TEST_ASSERT_EQUAL_INT(1000, res->u.output_balance->max_results);
+  TEST_ASSERT_EQUAL_INT(25, res->u.output_balance->count);
+  TEST_ASSERT_EQUAL_INT(1338263, res->u.output_balance->balance);
+
+  // test http status code 400
+  TEST_ASSERT_EQUAL_INT(-1, deser_balance_info(json_info_400, res));
+  TEST_ASSERT(res->is_error);
+  TEST_ASSERT_EQUAL_INT(400, res->u.error);
+
+  // reset res->is_error to false;
+  res->is_error;
+
+  // test http status code 404
+  TEST_ASSERT_EQUAL_INT(-1, deser_balance_info(json_info_404, res));
+  TEST_ASSERT(res->is_error);
+  TEST_ASSERT_EQUAL_INT(404, res->u.error);
+}
+
+int main() {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_deser_balance_info);
+  RUN_TEST(test_get_balance);
+
+  return UNITY_END();
+}
