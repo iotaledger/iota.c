01cfd2e2ba1b90e422976c8682d625dfc2c503d3
Sam Chen <sam.chen@iota.org>
2021-02-17 19:04:06 +0800

    Client - multi-parents for get_tips.

--- tests/client/api_v1/test_get_tips.c
+++ tests/client/api_v1/test_get_tips.c
@@ -12,50 +12,41 @@ void test_get_tips() {
       .port = 0  // use default port number
   };
 
-  // dynamic object
   res_tips_t* res_tips = res_tips_new();
   TEST_ASSERT_NOT_NULL(res_tips);
   TEST_ASSERT(get_tips(&ctx, res_tips) == 0);
   TEST_ASSERT(res_tips->is_error == false);
+  TEST_ASSERT(get_tips_id_count(res_tips) > 0);
   res_tips_free(res_tips);
-
-  // static object
-  res_tips_t res_tips_static = {};
-  TEST_ASSERT(get_tips(&ctx, &res_tips_static) == 0);
-  TEST_ASSERT(res_tips_static.is_error == false);
 }
 
 void test_deser_get_tips() {
   char const* const json_tips =
-      "{\"data\":{\"tip1MessageId\":\"42e089e73926b1b3dbf222ef2aea565645f442bc9ff74d0a86531ad6736882e2\","
-      "\"tip2MessageId\":\"ad2f6b896fc9ae6bd8b675a7c01ed53e389d406efd09b1f66a03b31c32240bd1\"}}";
+      "{\"data\":{\"tipMessageIds\":[\"0a7c22aa43620d938146bcfc94a40804b26a5aaf3913bdc51b2836c47650de5c\","
+      "\"3a3ca52a4c7d96fed4eef037db4421231f09e63a817bcf913f33c0806e565de9\","
+      "\"a714ca72bb21d1e69b49a272713b9a1edc4d5679695680b5f36b907c0ed0d445\","
+      "\"ea878b98a3eb38154993ea27d597e6cbb8fda0cd7b71cc2eb345b6c62140a6bf\"]}}";
 
-  // dynamic object
   res_tips_t* res_tips = res_tips_new();
   TEST_ASSERT_NOT_NULL(res_tips);
   TEST_ASSERT(deser_get_tips(json_tips, res_tips) == 0);
   TEST_ASSERT(res_tips->is_error == false);
-  TEST_ASSERT_EQUAL_MEMORY("42e089e73926b1b3dbf222ef2aea565645f442bc9ff74d0a86531ad6736882e2", res_tips->u.tips.tip1,
-                           STR_TIP_MSG_LEN);
-  TEST_ASSERT_EQUAL_MEMORY("ad2f6b896fc9ae6bd8b675a7c01ed53e389d406efd09b1f66a03b31c32240bd1", res_tips->u.tips.tip2,
-                           STR_TIP_MSG_LEN);
+  TEST_ASSERT_EQUAL_INT(4, get_tips_id_count(res_tips));
+  TEST_ASSERT_EQUAL_MEMORY("0a7c22aa43620d938146bcfc94a40804b26a5aaf3913bdc51b2836c47650de5c", get_tips_id(res_tips, 0),
+                           STR_TIP_MSG_ID_LEN);
+  TEST_ASSERT_EQUAL_MEMORY("3a3ca52a4c7d96fed4eef037db4421231f09e63a817bcf913f33c0806e565de9", get_tips_id(res_tips, 1),
+                           STR_TIP_MSG_ID_LEN);
+  TEST_ASSERT_EQUAL_MEMORY("a714ca72bb21d1e69b49a272713b9a1edc4d5679695680b5f36b907c0ed0d445", get_tips_id(res_tips, 2),
+                           STR_TIP_MSG_ID_LEN);
+  TEST_ASSERT_EQUAL_MEMORY("ea878b98a3eb38154993ea27d597e6cbb8fda0cd7b71cc2eb345b6c62140a6bf", get_tips_id(res_tips, 3),
+                           STR_TIP_MSG_ID_LEN);
   res_tips_free(res_tips);
-
-  // static object
-  res_tips_t res_tips_static = {};
-  TEST_ASSERT(deser_get_tips(json_tips, &res_tips_static) == 0);
-  TEST_ASSERT(res_tips_static.is_error == false);
-  TEST_ASSERT_EQUAL_MEMORY("42e089e73926b1b3dbf222ef2aea565645f442bc9ff74d0a86531ad6736882e2",
-                           res_tips_static.u.tips.tip1, STR_TIP_MSG_LEN);
-  TEST_ASSERT_EQUAL_MEMORY("ad2f6b896fc9ae6bd8b675a7c01ed53e389d406efd09b1f66a03b31c32240bd1",
-                           res_tips_static.u.tips.tip2, STR_TIP_MSG_LEN);
 }
 
 void test_deser_tips_err() {
   char const* const json_err =
       "{\"error\":{\"code\":\"service_unavailable\",\"message\":\"unable to handle the request\"}}";
 
-  // dynamic object
   res_tips_t* res_tips = res_tips_new();
   TEST_ASSERT_NOT_NULL(res_tips);
   TEST_ASSERT(deser_get_tips(json_err, res_tips) == 0);
@@ -63,15 +54,6 @@ void test_deser_tips_err() {
   TEST_ASSERT_EQUAL_STRING(res_tips->u.error->code, "service_unavailable");
   TEST_ASSERT_EQUAL_STRING(res_tips->u.error->msg, "unable to handle the request");
   res_tips_free(res_tips);
-
-  // static object
-  res_tips_t res_tips_static = {};
-  TEST_ASSERT(deser_get_tips(json_err, &res_tips_static) == 0);
-  TEST_ASSERT(res_tips_static.is_error == true);
-  TEST_ASSERT_EQUAL_STRING(res_tips_static.u.error->code, "service_unavailable");
-  TEST_ASSERT_EQUAL_STRING(res_tips_static.u.error->msg, "unable to handle the request");
-  // error object need to free, it is dynamic allocation
-  res_err_free(res_tips_static.u.error);
 }
 
 int main() {
