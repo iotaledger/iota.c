272d8290a12ccbe08112c0af7afa23ef3e264b19
Sam Chen <sam.chen@iota.org>
2021-03-13 16:12:32 +0800

    Crypto - added blake2 and hmacsha on mbed os.

--- src/crypto/iota_crypto.c
+++ src/crypto/iota_crypto.c
@@ -3,7 +3,12 @@
 
 #ifdef CRYPTO_USE_SODIUM
 #include <sodium.h>
-#else
+#elif __MBED__  // mbed os with mbedtls
+#include <string.h>
+#include "blake2.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/md.h"
+#else  // linux with openssl
 #include <openssl/hmac.h>
 #include <string.h>
 #include <sys/random.h>
@@ -16,6 +21,12 @@
 void iota_crypto_randombytes(uint8_t *const buf, const size_t len) {
 #if CRYPTO_USE_SODIUM
   randombytes_buf((void *const)buf, len);
+#elif __MBED__
+  // TODO use (T)RNG or mbed PSA
+  srand((unsigned int)time(NULL));
+  for (size_t l = 0; l < len; l++) {
+    buf[l] = (uint8_t)rand();
+  }
 #else
   ssize_t ret = -1;
   while (ret == -1) {
@@ -28,6 +39,8 @@ void iota_crypto_randombytes(uint8_t *const buf, const size_t len) {
 void iota_crypto_keypair(uint8_t const seed[], iota_keypair_t *keypair) {
 #if CRYPTO_USE_SODIUM
   crypto_sign_seed_keypair(keypair->pub, keypair->priv, seed);
+#elif __MBED__
+  // TODO
 #else
   ed25519_public_key pub;
   ed25519_publickey(seed, pub);
@@ -41,6 +54,9 @@ int iota_crypto_sign(uint8_t const priv_key[], uint8_t msg[], size_t msg_len, ui
 #if CRYPTO_USE_SODIUM
   unsigned long long sign_len = ED_SIGNATURE_BYTES;
   return crypto_sign_ed25519_detached(signature, &sign_len, msg, msg_len, priv_key);
+#elif __MBED__
+  // TODO
+  return 0;
 #else
   ed25519_sign(msg, msg_len, priv_key, priv_key + 32, signature);
   return 0;
@@ -50,6 +66,13 @@ int iota_crypto_sign(uint8_t const priv_key[], uint8_t msg[], size_t msg_len, ui
 int iota_crypto_hmacsha256(uint8_t const secret_key[], uint8_t msg[], size_t msg_len, uint8_t auth[]) {
 #if CRYPTO_USE_SODIUM
   return crypto_auth_hmacsha256(auth, msg, msg_len, secret_key);
+#elif __MBED__
+  int ret = -1;
+  const mbedtls_md_info_t *md_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA256);
+  if (md_info) {
+    ret = mbedtls_md_hmac(md_info, secret_key, 32, msg, msg_len, auth);
+  }
+  return ret;
 #else
   uint8_t *hash = HMAC(EVP_sha256(), secret_key, 32, (const unsigned char *)msg, msg_len, NULL, NULL);
   memcpy(auth, hash, 32);
@@ -60,6 +83,14 @@ int iota_crypto_hmacsha256(uint8_t const secret_key[], uint8_t msg[], size_t msg
 int iota_crypto_hmacsha512(uint8_t const secret_key[], uint8_t msg[], size_t msg_len, uint8_t auth[]) {
 #if CRYPTO_USE_SODIUM
   return crypto_auth_hmacsha512(auth, msg, msg_len, secret_key);
+#elif __MBED__
+  int ret = -1;
+  const mbedtls_md_info_t *md_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA512);
+  if (md_info) {
+    ret = mbedtls_md_hmac(md_info, secret_key, 32, msg, msg_len, auth);
+  }
+  return ret;
+  return 0;
 #else
   uint8_t *hash = HMAC(EVP_sha512(), secret_key, 32, (const unsigned char *)msg, msg_len, NULL, NULL);
   memcpy(auth, hash, 64);
@@ -70,6 +101,8 @@ int iota_crypto_hmacsha512(uint8_t const secret_key[], uint8_t msg[], size_t msg
 int iota_blake2b_sum(uint8_t const msg[], size_t msg_len, uint8_t out[], size_t out_len) {
 #if CRYPTO_USE_SODIUM
   return crypto_generichash_blake2b(out, out_len, msg, msg_len, NULL, 0);
+#elif __MBED__
+  return blake2b(out, out_len, msg, msg_len, NULL, 0);
 #else
   return blake2b(out, out_len, msg, msg_len, NULL, 0);
 #endif
