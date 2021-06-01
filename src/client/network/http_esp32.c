808a590c51a8e24e05ff91cbca0f96ae8f6145fd
Sam Chen <sam.chen@iota.org>
2021-06-01 22:21:23 +0800

    Client - enabled chunked data on the esp32 http client.

--- src/client/network/http_esp32.c
+++ src/client/network/http_esp32.c
@@ -25,40 +25,33 @@ static esp_err_t http_event_handler(esp_http_client_event_t* evt) {
 
   switch (evt->event_id) {
     case HTTP_EVENT_ERROR:
-      // ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+      ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
       break;
     case HTTP_EVENT_ON_CONNECTED:
-      // ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
       break;
     case HTTP_EVENT_HEADER_SENT:
-      // ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+      ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
       break;
     case HTTP_EVENT_ON_HEADER:
-      // ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
       break;
     case HTTP_EVENT_ON_DATA:
-      // ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-      /*
-       *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
-       *  However, event handler can also be used in case chunked encoding is used.
-       */
-      if (!esp_http_client_is_chunked_response(evt->client)) {
-        // If user_data buffer is configured, copy the response into the buffer
-        if (buf) {
-          byte_buf_append(buf, evt->data, evt->data_len);
-        } else {
-          ESP_LOGE(TAG, "NULL buffer");
-        }
-        output_len += evt->data_len;
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+      // copy the response data into the buffer
+      if (buf) {
+        byte_buf_append(buf, evt->data, evt->data_len);
+      } else {
+        ESP_LOGE(TAG, "NULL buffer");
       }
-
+      output_len += evt->data_len;
       break;
     case HTTP_EVENT_ON_FINISH:
-      // ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
       output_len = 0;
       break;
     case HTTP_EVENT_DISCONNECTED:
-      // ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+      ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
       {
         int mbedtls_err = 0;
         esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
@@ -72,22 +65,6 @@ static esp_err_t http_event_handler(esp_http_client_event_t* evt) {
   }
   return ESP_OK;
 }
-
-#if 0
-static void http_download_chunk(void) {
-  esp_http_client_config_t config = {
-      .url = "http://httpbin.org/stream-bytes/8912",
-      .event_handler = http_event_handler,
-  };
-  esp_http_client_handle_t client = esp_http_client_init(&config);
-  esp_err_t err = esp_http_client_perform(client);
-
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-  }
-  esp_http_client_cleanup(client);
-}
-#endif
 
 static void init_config(esp_http_client_config_t* esp, http_client_config_t const* const conf) {
   esp->url = conf->url;
