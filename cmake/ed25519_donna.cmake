34514eecaf6f75c31c35f9128a1156ea65086a75
Sam Chen <sam.chen@iota.org>
2021-09-16 22:13:00 +0800

    Crypto - support mbedtls on Linux. (#149)
    * Crypto - support mbedtls on Linux.
    
    * CI - added mbedtls build into jobs
    
    * GH - update CODEOWNERS

--- cmake/ed25519_donna.cmake
+++ cmake/ed25519_donna.cmake
@@ -6,8 +6,6 @@
 if (NOT __ED25519_INCLUDE)
   set(__ED25519_INCLUDE TRUE)
 
-  # find_package(OpenSSL REQUIRED)
-
   ExternalProject_Add(
     ed25519_download
     PREFIX ${PROJECT_BINARY_DIR}/ed25519
@@ -30,9 +28,13 @@ if (NOT __ED25519_INCLUDE)
   file(WRITE ${ed25519_cmake_dir}/CMakeLists.txt
     "cmake_minimum_required(VERSION 3.5)\n"
     "project(ed25519 C)\n"
-    "# find_package(OpenSSL REQUIRED)\n"
-    "message(\"OpenSSL include dir: ${OPENSSL_INCLUDE_DIR}\")\n"
-    "message(\"OpenSSL libraries: ${OPENSSL_LIBRARIES}\")\n"
+    "option(CRYPTO_MBEDTLS \"Use mbedtls hash function\" OFF)\n"
+    "option(CRYPTO_OPENSSL \"Use openssl hash function\" OFF)\n"
+    "if(CRYPTO_OPENSSL)\n"
+    "  find_package(OpenSSL REQUIRED)\n"
+    "  message(\"OpenSSL include dir: ${OPENSSL_INCLUDE_DIR}\")\n"
+    "  message(\"OpenSSL libraries: ${OPENSSL_LIBRARIES}\")\n"
+    "endif()\n"
     "add_library(ed25519_donna STATIC)\n"
     "target_sources(\n"
     "ed25519_donna\n"
@@ -61,25 +63,78 @@ if (NOT __ED25519_INCLUDE)
     "  \"${ed25519_src_dir}/modm-donna-32bit.h\"\n"
     "  \"${ed25519_src_dir}/modm-donna-64bit.h\"\n"
     "PUBLIC \"${ed25519_src_dir}/ed25519.h\")\n"
-    "target_link_libraries(ed25519_donna PUBLIC ${OPENSSL_LIBRARIES})\n"
-    "target_include_directories(ed25519_donna PUBLIC ${ed25519_src_dir} ${OPENSSL_INCLUDE_DIR})\n"
+    "if(CRYPTO_OPENSSL)\n"
+    "  target_link_libraries(ed25519_donna INTERFACE ${OPENSSL_LIBRARIES})\n"
+    "  target_include_directories(ed25519_donna PUBLIC ${ed25519_src_dir} ${OPENSSL_INCLUDE_DIR})\n"
+    "endif()\n"
+    "if(CRYPTO_MBEDTLS)\n"
+    "  target_compile_definitions(ed25519_donna PRIVATE ED25519_CUSTOMHASH)\n"
+    "  target_compile_definitions(ed25519_donna PRIVATE ED25519_CUSTOMRANDOM)\n"
+    "  target_link_libraries(ed25519_donna INTERFACE mbedcrypto mbedtls mbedx509)\n"
+    "  target_include_directories(ed25519_donna PUBLIC ${ed25519_src_dir} ${CMAKE_INSTALL_PREFIX}/include)\n"
+    "  file(WRITE ${ed25519_src_dir}/ed25519-hash-custom.h"
+    "    \"#include <stdlib.h>\n\""
+    "    \"#include \\\"mbedtls/sha512.h\\\"\n\""
+    "    \"typedef mbedtls_sha512_context ed25519_hash_context;\n\""
+    "    \"void ed25519_hash_init(ed25519_hash_context *ctx){mbedtls_sha512_init(ctx); mbedtls_sha512_starts(ctx, 0);}\n\""
+    "    \"void ed25519_hash_update(ed25519_hash_context *ctx, const uint8_t *in, size_t inlen){mbedtls_sha512_update(ctx, in, inlen);}\n\""
+    "    \"void ed25519_hash_final(ed25519_hash_context *ctx, uint8_t *hash){mbedtls_sha512_finish(ctx, hash); mbedtls_sha512_free(ctx);}\n\""
+    "    \"void ed25519_hash(uint8_t *hash, const uint8_t *in, size_t inlen){mbedtls_sha512(in, inlen, hash, 0);}\n\""
+    "  )\n"
+    "  file(WRITE ${ed25519_src_dir}/ed25519-randombytes-custom.h"
+    "    \"#include <stdlib.h>\n\""
+    "    \"#include \\\"mbedtls/ctr_drbg.h\\\"\n\""
+    "    \"#include \\\"mbedtls/entropy.h\\\"\n\""
+    "    \"void ED25519_FN(ed25519_randombytes_unsafe) (void *p, size_t len){\n\""
+    "    \"  int ret = 0; mbedtls_ctr_drbg_context drbg; mbedtls_entropy_context ent;\n\""
+    "    \"  mbedtls_ctr_drbg_init(&drbg);\n\""
+    "    \"  mbedtls_entropy_init(&ent);\n\""
+    "    \"  ret = mbedtls_ctr_drbg_seed(&drbg, mbedtls_entropy_func, &ent, (unsigned char const *)\\\"CTR_DRBG\\\", 8);\n\""
+    "    \"  if (ret == 0) { mbedtls_ctr_drbg_random(&drbg, p, len); }\n\""
+    "    \"  mbedtls_entropy_free(&ent);\n\""
+    "    \"  mbedtls_ctr_drbg_free(&drbg);\n\""
+    "    \"}\n\""
+    "  )\n"
+    "endif()\n"
     "install(TARGETS ed25519_donna DESTINATION ${ed25519_install_lib})\n"
     "install(FILES ${ed25519_src_dir}/ed25519.h DESTINATION ${ed25519_install_include})\n"
   )
 
-  ExternalProject_Add(
-    ext_ed25519
-    PREFIX ${PROJECT_BINARY_DIR}/ed25519
-    DOWNLOAD_COMMAND ""
-    BUILD_IN_SOURCE TRUE
-    CMAKE_ARGS
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-      -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
-      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-    # for debug
-    # LOG_CONFIGURE 1
-    # LOG_INSTALL 1
-  )
-  add_dependencies(ext_ed25519 ed25519_download)
+  if(__MBEDTLS_INCLUDED)
+    ExternalProject_Add(
+      ext_ed25519
+      PREFIX ${PROJECT_BINARY_DIR}/ed25519
+      DOWNLOAD_COMMAND ""
+      BUILD_IN_SOURCE TRUE
+      CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+        -DCRYPTO_MBEDTLS:BOOL=TRUE
+      # for debug
+      # LOG_CONFIGURE 1
+      # LOG_INSTALL 1
+    )
+    add_dependencies(ext_ed25519 ed25519_download ext_mbedtls)
+
+  else()
+    find_package(OpenSSL REQUIRED)
+    ExternalProject_Add(
+      ext_ed25519
+      PREFIX ${PROJECT_BINARY_DIR}/ed25519
+      DOWNLOAD_COMMAND ""
+      BUILD_IN_SOURCE TRUE
+      CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+        -DCRYPTO_OPENSSL:BOOL=TRUE
+      # for debug
+      # LOG_CONFIGURE 1
+      # LOG_INSTALL 1
+    )
+    add_dependencies(ext_ed25519 ed25519_download)
+
+  endif()
 
 endif()
