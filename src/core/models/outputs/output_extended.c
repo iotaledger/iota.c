78b6e1f425a6d55b8fe0c8e8c8e4b1b7d1a890ee
Matjaz Verbole <verbole@gmail.com>
2021-12-14 14:13:02 +0100

    Implemented some functions for extended output (new, free, serialize, serialize length).

--- src/core/models/outputs/output_extended.c
+++ src/core/models/outputs/output_extended.c
@@ -2,3 +2,169 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "core/models/outputs/output_extended.h"
+#include "core/address.h"
+#include "uthash.h"
+
+output_extended_t* output_extended_new(void* address, uint64_t amount, native_tokens_t** tokens, void* feature_blocks) {
+  if (address == NULL) {
+    printf("[%s:%d] invalid parameters\n", __func__, __LINE__);
+    return NULL;
+  }
+
+  if (amount < 1000000) {
+    printf("[%s:%d] dust allowance amount must be at least 1Mi\n", __func__, __LINE__);
+    return NULL;
+  }
+
+  output_extended_t* oe = malloc(sizeof(output_extended_t));
+  if (!oe) {
+    return NULL;
+  }
+
+  switch (((address_t*)address)->type) {
+    case ADDRESS_TYPE_ED25519:
+      oe->address = malloc(ADDRESS_ED25519_BYTES);
+      if (!oe->address) {
+        printf("[%s:%d] OOM\n", __func__, __LINE__);
+        output_extended_free(oe);
+        return NULL;
+      }
+      memcpy(oe->address, ((address_t*)address)->address, ADDRESS_ED25519_BYTES);
+      break;
+    case ADDRESS_TYPE_ALIAS:
+      oe->address = malloc(ADDRESS_ALIAS_BYTES);
+      if (!oe->address) {
+        printf("[%s:%d] OOM\n", __func__, __LINE__);
+        output_extended_free(oe);
+        return NULL;
+      }
+      memcpy(oe->address, ((address_t*)address)->address, ADDRESS_ALIAS_BYTES);
+      break;
+    case ADDRESS_TYPE_NFT:
+      oe->address = malloc(ADDRESS_NFT_BYTES);
+      if (!oe->address) {
+        printf("[%s:%d] OOM\n", __func__, __LINE__);
+        output_extended_free(oe);
+        return NULL;
+      }
+      memcpy(oe->address, ((address_t*)address)->address, ADDRESS_NFT_BYTES);
+      break;
+    default:
+      printf("[%s:%d] unknown address type\n", __func__, __LINE__);
+      output_extended_free(oe);
+      return NULL;
+  }
+
+  oe->amount = amount;
+
+  if (tokens != NULL) {
+    // oe->native_tokens = native_tokens_new();
+
+    native_tokens_t *token, *token_tmp;
+    HASH_ITER(hh, *tokens, token, token_tmp) {
+      /*int res = native_tokens_add(&oe->native_tokens, token->token_id, uint256_to_string(token->amount));
+      if (res == -1) {
+        printf("[%s:%d] can not add native token to extended output\n", __func__, __LINE__);
+        output_extended_free(oe);
+        return NULL;
+      }*/
+    }
+  }
+
+  return oe;
+}
+
+void output_extended_free(output_extended_t* oe) {
+  if (oe) {
+    if (oe->address) {
+      free(oe->address);
+    }
+    if (oe->native_tokens) {
+      // native_tokens_free(&oe->native_tokens);
+    }
+    if (oe->feature_blocks) {
+      // feature_blocks_free(oe->feature_blocks);
+    }
+    free(oe);
+  }
+}
+
+size_t output_extended_serialize_length(output_extended_t* oe) {
+  if (oe == NULL) {
+    printf("[%s:%d] invalid parameters\n", __func__, __LINE__);
+    return 0;
+  }
+
+  size_t length = 0;
+
+  // output type
+  length += sizeof(uint8_t);
+  // address
+  length += address_serialized_len(oe->address);
+  // amount
+  length += sizeof(uint64_t);
+  // native tokens count
+  length += sizeof(uint16_t);
+  // native tokens
+  // length += native_tokens_serialize_length(oe->native_tokens);
+  // feature blocks count
+  length += sizeof(uint8_t);
+  // feature blocks
+  // length += feature_blocks_serialize_length(oe->native_tokens);
+
+  return length;
+}
+
+size_t output_extended_serialize(output_extended_t* oe, byte_t buf[]) {
+  if (oe == NULL || oe->address == NULL || buf == NULL) {
+    printf("[%s:%d] invalid parameters\n", __func__, __LINE__);
+    return 0;
+  }
+
+  byte_t* offset = buf;
+
+  // fill-in output type, set to value 3 to denote a Extended Output
+  memset(offset, 3, sizeof(uint8_t));
+  offset += sizeof(uint8_t);
+
+  // address
+  switch (((address_t*)(oe->address))->type) {
+    case ADDRESS_TYPE_ED25519:
+      memcpy(offset, oe->address, ADDRESS_ED25519_BYTES);
+      offset += ADDRESS_ED25519_BYTES;
+      break;
+    case ADDRESS_TYPE_ALIAS:
+      memcpy(offset, oe->address, ADDRESS_ALIAS_BYTES);
+      offset += ADDRESS_TYPE_ALIAS;
+      break;
+    case ADDRESS_TYPE_NFT:
+      memcpy(offset, oe->address, ADDRESS_NFT_BYTES);
+      offset += ADDRESS_NFT_BYTES;
+      break;
+  }
+
+  // amount
+  memset(offset, oe->amount, sizeof(uint64_t));
+
+  // Native Tokens
+  if (oe->native_tokens) {
+    /*memset(offset, native_tokens_len(oe->native_tokens), sizeof(uint16_t));
+    offset += sizeof(uint16_t);
+    offset += native_tokens_serialize(oe->native_tokens);*/
+  } else {
+    memset(offset, 0, sizeof(uint16_t));
+    offset += sizeof(uint16_t);
+  }
+
+  // Feature Blocks
+  if (oe->feature_blocks) {
+    /*memset(offset, feature_blocks_len(oe->native_tokens), sizeof(uint8_t));
+    offset += sizeof(uint8_t);
+    offset += feature_blocks_serialize(oe->native_tokens);*/
+  } else {
+    memset(offset, 0, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
+  }
+
+  return (offset - buf) / sizeof(byte_t);
+}
--- src/core/models/outputs/output_extended.h
+++ src/core/models/outputs/output_extended.h
@@ -13,10 +13,54 @@
  *
  */
 typedef struct {
-  void* address;                  ///< The deposit address
-  uint64_t amount;                ///< The amount of IOTA tokens
-  native_tokens_t native_tokens;  ///< The native tokens
-  void* feature_blocks;           ///< The feature blocks which modulate the constrants on the output
+  void* address;                   ///< Deposit address
+  uint64_t amount;                 ///< The amount of IOTA tokens to held by the output
+  native_tokens_t* native_tokens;  ///< The native tokens held by the output
+  void* feature_blocks;            ///< The feature blocks which modulate the constraints on the output
 } output_extended_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Create new Extended Output object.
+ *
+ * @param[in] address Deposit address (Ed25519, Alias or NFT address)
+ * @param[in] amount The amount of IOTA tokens to held by the output
+ * @param[in] tokens Set of native tokens held by the output
+ * @param[in] feature_blocks Set of feature blocks
+ *
+ * @return output_extended_t* or NULL on failure
+ */
+output_extended_t* output_extended_new(void* address, uint64_t amount, native_tokens_t** tokens, void* feature_blocks);
+
+/**
+ * @brief Free Extended Output object.
+ *
+ * @param[in] oe Extended Output object.
+ */
+void output_extended_free(output_extended_t* oe);
+
+/**
+ * @brief Get the length of a serialized Extended Output
+ *
+ * @param[in] oe Extended Output object.
+ * @return size_t The number of bytes of serialized data
+ */
+size_t output_extended_serialize_length(output_extended_t* oe);
+
+/**
+ * @brief Serialize Extended Output to a buffer
+ *
+ * @param[in] oe Extended Output object.
+ * @param[out] buf A buffer holds the serialized data
+ * @return size_t number of bytes written to the buffer
+ */
+size_t output_extended_serialize(output_extended_t* oe, byte_t buf[]);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
