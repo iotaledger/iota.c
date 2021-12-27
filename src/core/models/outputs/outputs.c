3e7d470ae4a64866a09a36aecf4b72c46384787e
Matjaz Verbole <verbole@gmail.com>
2021-12-27 13:22:20 +0100

    Implemented outputs module

--- src/core/models/outputs/outputs.c
+++ src/core/models/outputs/outputs.c
@@ -3,73 +3,229 @@
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "core/models/outputs/output_alias.h"
+#include "core/models/outputs/output_extended.h"
+#include "core/models/outputs/output_foundry.h"
+#include "core/models/outputs/output_nft.h"
 #include "core/models/outputs/outputs.h"
+#include "utlist.h"
 
-#define UTXO_OUTPUT_MIN_COUNT 0
-#define UTXO_OUTPUT_MAX_COUNT 126
+#define UTXO_OUTPUT_MAX_COUNT 127
 
-int utxo_outputs_add(outputs_ht **ht, output_type_t type, byte_t addr[], uint64_t amount) {
-  if (type == OUTPUT_DUST_ALLOWANCE && amount < 1000000) {
-    printf("[%s:%d] dust allowance amount must at least 1Mi\n", __func__, __LINE__);
-    return -1;
+utxo_outputs_list_t *utxo_outputs_new() { return NULL; }
+
+void utxo_outputs_free(utxo_outputs_list_t *outputs) {
+  if (outputs) {
+    utxo_outputs_list_t *elm, *tmp;
+    LL_FOREACH_SAFE(outputs, elm, tmp) {
+      switch (elm->output->output_type) {
+        case OUTPUT_EXTENDED:
+          output_extended_free(elm->output->output);
+          break;
+        case OUTPUT_ALIAS:
+          output_alias_free(elm->output->output);
+          break;
+        case OUTPUT_FOUNDRY:
+          output_foundry_free(elm->output->output);
+          break;
+        case OUTPUT_NFT:
+          output_nft_free(elm->output->output);
+          break;
+      }
+      free(elm->output);
+      LL_DELETE(outputs, elm);
+      free(elm);
+    }
   }
-
-  if (utxo_outputs_count(ht) >= UTXO_OUTPUT_MAX_COUNT) {
-    printf("[%s:%d] output count must be < 127\n", __func__, __LINE__);
-    return -1;
-  }
-
-  outputs_ht *elm = utxo_outputs_find_by_addr(ht, addr);
-  if (elm) {
-    printf("[%s:%d] address exists\n", __func__, __LINE__);
-    return -1;
-  }
-
-  elm = malloc(sizeof(outputs_ht));
-  if (elm == NULL) {
-    printf("[%s:%d] OOM\n", __func__, __LINE__);
-    return -1;
-  }
-  elm->output_type = type;
-  memcpy(elm->address, addr, ADDRESS_ED25519_BYTES);
-  elm->amount = amount;
-  HASH_ADD(hh, *ht, address, ADDRESS_ED25519_BYTES, elm);
-  return 0;
 }
 
-size_t utxo_outputs_serialization(outputs_ht **ht, byte_t buf[]) {
-  outputs_ht *elm, *tmp;
-  size_t byte_count = 0;
-  uint8_t elm_count = 0;
-  HASH_ITER(hh, *ht, elm, tmp) {
-    // output type
-    memset(buf + byte_count, elm->output_type, sizeof(byte_t));
-    byte_count += sizeof(byte_t);
-
-    // address type, set to value 0 to denote an ed25519.
-    memset(buf + byte_count, ADDRESS_TYPE_ED25519, sizeof(byte_t));
-    byte_count += sizeof(byte_t);
-
-    // ed25519 address
-    memcpy(buf + byte_count, elm->address, ADDRESS_ED25519_BYTES);
-    byte_count += ADDRESS_ED25519_BYTES;
-
-    // amount
-    memcpy(buf + byte_count, &elm->amount, sizeof(elm->amount));
-    byte_count += sizeof(elm->amount);
-
-    elm_count++;
+int utxo_outputs_add(utxo_outputs_list_t **outputs, utxo_output_type_t type, void *output) {
+  if (output == NULL) {
+    printf("[%s:%d] invalid parameters\n", __func__, __LINE__);
+    return -1;
   }
 
-  if (byte_count != (elm_count * UTXO_OUTPUT_SERIALIZED_BYTES)) {
-    printf("[%s:%d] offset error\n", __func__, __LINE__);
+  if (utxo_outputs_count(*outputs) >= UTXO_OUTPUT_MAX_COUNT) {
+    printf("[%s:%d] output count must be <= 127\n", __func__, __LINE__);
+    return -1;
+  }
+
+  utxo_outputs_list_t *next = malloc(sizeof(utxo_outputs_list_t));
+  if (next) {
+    next->output = malloc(sizeof(utxo_output_t));
+    if (next->output) {
+      next->output->output_type = type;
+      switch (next->output->output_type) {
+        case OUTPUT_EXTENDED:
+          next->output->output = malloc(sizeof(output_extended_t));
+          memcpy(next->output->output, output, sizeof(output_extended_t));
+          break;
+        case OUTPUT_ALIAS:
+          next->output->output = malloc(sizeof(output_alias_t));
+          memcpy(next->output->output, output, sizeof(output_alias_t));
+          break;
+        case OUTPUT_FOUNDRY:
+          next->output->output = malloc(sizeof(output_foundry_t));
+          memcpy(next->output->output, output, sizeof(output_foundry_t));
+          break;
+        case OUTPUT_NFT:
+          next->output->output = malloc(sizeof(output_nft_t));
+          memcpy(next->output->output, output, sizeof(output_nft_t));
+          break;
+      }
+      if (next->output->output) {
+        LL_APPEND(*outputs, next);
+        return 0;
+      } else {
+        free(next);
+      }
+    }
+  }
+
+  return -1;
+}
+
+uint16_t utxo_outputs_count(utxo_outputs_list_t *outputs) {
+  utxo_outputs_list_t *elm = NULL;
+  uint16_t len = 0;
+  if (outputs) {
+    LL_COUNT(outputs, elm, len);
+    return len;
+  }
+  return len;
+}
+
+utxo_output_t *utxo_outputs_get(utxo_outputs_list_t *outputs, uint16_t index) {
+  uint16_t count = 0;
+  utxo_outputs_list_t *elm;
+  if (outputs) {
+    LL_FOREACH(outputs, elm) {
+      if (count == index) {
+        return elm->output;
+      }
+      count++;
+    }
+  }
+  return NULL;
+}
+
+size_t utxo_outputs_serialize_len(utxo_outputs_list_t *outputs) {
+  if (outputs == NULL) {
+    printf("[%s:%d] invalid parameters\n", __func__, __LINE__);
     return 0;
   }
 
-  return byte_count;
+  size_t len = 0;
+
+  // Outputs layout: Outputs Count + Outputs
+  // uint16_t is a number of output entries
+  len += sizeof(uint16_t);
+
+  utxo_outputs_list_t *elm;
+  size_t output_len;
+  LL_FOREACH(outputs, elm) {
+    switch (elm->output->output_type) {
+      case OUTPUT_EXTENDED:
+        output_len = output_extended_serialize_len(elm->output->output);
+        break;
+      case OUTPUT_ALIAS:
+        output_len = output_alias_serialize_len(elm->output->output);
+        break;
+      case OUTPUT_FOUNDRY:
+        output_len = output_foundry_serialize_len(elm->output->output);
+        break;
+      case OUTPUT_NFT:
+        output_len = output_nft_serialize_len(elm->output->output);
+        break;
+    }
+    if (output_len == 0) {
+      printf("[%s:%d] can not get outputs serialized len\n", __func__, __LINE__);
+      return 0;
+    }
+    len += output_len;
+  }
+
+  return len;
 }
 
+size_t utxo_outputs_serialize(utxo_outputs_list_t *outputs, byte_t buf[], size_t buf_len) {
+  if (outputs == NULL || buf == NULL || buf_len == 0) {
+    printf("[%s:%d] invalid parameters\n", __func__, __LINE__);
+    return 0;
+  }
+
+  size_t expected_bytes = utxo_outputs_serialize_len(outputs);
+  if (buf_len < expected_bytes) {
+    printf("[%s:%d] buffer size is insufficient\n", __func__, __LINE__);
+    return 0;
+  }
+  if (expected_bytes == 0) {
+    printf("[%s:%d] outputs serialized length is zero\n", __func__, __LINE__);
+    return 0;
+  }
+
+  byte_t *offset = buf;
+
+  // number of outputs entries
+  uint16_t output_count = utxo_outputs_count(outputs);
+  memcpy(offset, &output_count, sizeof(uint16_t));
+  offset += sizeof(uint16_t);
+
+  // outputs
+  utxo_outputs_list_t *elm;
+  LL_FOREACH(outputs, elm) {
+    switch (elm->output->output_type) {
+      case OUTPUT_EXTENDED:
+        offset +=
+            output_extended_serialize(elm->output->output, offset, output_extended_serialize_len(elm->output->output));
+        break;
+      case OUTPUT_ALIAS:
+        offset += output_alias_serialize(elm->output->output, offset, output_alias_serialize_len(elm->output->output));
+        break;
+      case OUTPUT_FOUNDRY:
+        offset +=
+            output_foundry_serialize(elm->output->output, offset, output_foundry_serialize_len(elm->output->output));
+        break;
+      case OUTPUT_NFT:
+        offset += output_nft_serialize(elm->output->output, offset, output_nft_serialize_len(elm->output->output));
+        break;
+    }
+  }
+
+  return expected_bytes;
+}
+
+void utxo_outputs_print(utxo_outputs_list_t *outputs) {
+  utxo_outputs_list_t *elm;
+  uint8_t index = 0;
+  printf("UTXO Outputs:[\n");
+  printf("Outputs Count: %d\n", utxo_outputs_count(outputs));
+  if (outputs) {
+    LL_FOREACH(outputs, elm) {
+      printf("#%d ", index);
+      switch (elm->output->output_type) {
+        case OUTPUT_EXTENDED:
+          output_extended_print(elm->output->output);
+          break;
+        case OUTPUT_ALIAS:
+          output_alias_print(elm->output->output);
+          break;
+        case OUTPUT_FOUNDRY:
+          output_foundry_print(elm->output->output);
+          break;
+        case OUTPUT_NFT:
+          output_nft_print(elm->output->output);
+          break;
+      }
+      index++;
+    }
+  }
+  printf("]\n");
+}
+
+/*
 void utxo_outputs_print(outputs_ht **ht) {
   outputs_ht *elm, *tmp;
   printf("utxo_outputs: [\n");
@@ -79,4 +235,4 @@ void utxo_outputs_print(outputs_ht **ht) {
     dump_hex(elm->address, ADDRESS_ED25519_BYTES);
   }
   printf("]\n");
-}
+}*/
