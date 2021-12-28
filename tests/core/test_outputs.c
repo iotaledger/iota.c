a3b0d2ab1e8558a82b9538942f03e36e0ec08096
Matjaz Verbole <matjaz.verbole@iota.org>
2021-12-28 10:15:23 +0100

    Updated outputs unit test to include foundry output check

--- tests/core/test_outputs.c
+++ tests/core/test_outputs.c
@@ -113,6 +113,65 @@ static output_alias_t* create_output_alias() {
   return output;
 }
 
+static output_foundry_t* create_output_foundry() {
+  byte_t token_id1[NATIVE_TOKEN_ID_BYTES] = {
+      0xBA, 0x26, 0x7E, 0x59, 0xE5, 0x31, 0x77, 0xB3, 0x2A, 0xA9, 0xBF, 0xE,  0x56, 0x31, 0x18, 0xC9, 0xE0, 0xAD, 0xD,
+      0x76, 0x88, 0x7B, 0x65, 0xFD, 0x58, 0x75, 0xB7, 0x13, 0x29, 0x73, 0x5B, 0x94, 0x2B, 0x81, 0x6A, 0x7F, 0xE6, 0x79};
+  byte_t token_id2[NATIVE_TOKEN_ID_BYTES] = {
+      0xDD, 0xA7, 0xC5, 0x79, 0x47, 0x9E, 0xC, 0x93, 0xCE, 0xA7, 0x93, 0x95, 0x41, 0xF8, 0x93, 0x4D, 0xF,  0x7E, 0x3A,
+      0x4,  0xCA, 0x52, 0xF8, 0x8B, 0x9B, 0x0, 0x25, 0xC0, 0xBE, 0x4A, 0xF6, 0x23, 0x59, 0x98, 0x6F, 0x64, 0xEF, 0x14};
+  byte_t token_id3[NATIVE_TOKEN_ID_BYTES] = {
+      0x74, 0x6B, 0xA0, 0xD9, 0x51, 0x41, 0xCB, 0x5B, 0x4B, 0xF7, 0x1C, 0x9D, 0x3E, 0x76, 0x81, 0xBE, 0xB6, 0xA3, 0xAE,
+      0x5A, 0x6D, 0x7C, 0x89, 0xD0, 0x98, 0x42, 0xDF, 0x86, 0x27, 0x5A, 0xF,  0x9,  0xCB, 0xE0, 0xF9, 0x1A, 0x6C, 0x6B};
+
+  // create random ED25519 address
+  address_t addr = {};
+  addr.type = ADDRESS_TYPE_ED25519;
+  iota_crypto_randombytes(addr.address, ADDRESS_ED25519_BYTES);
+
+  // create Native Tokens
+  native_tokens_t* native_tokens = native_tokens_new();
+  uint256_t* amount1 = uint256_from_str("111111111");
+  native_tokens_add(&native_tokens, token_id1, amount1);
+  uint256_t* amount2 = uint256_from_str("222222222");
+  native_tokens_add(&native_tokens, token_id2, amount2);
+  uint256_t* amount3 = uint256_from_str("333333333");
+  native_tokens_add(&native_tokens, token_id3, amount3);
+
+  // create random token tag
+  byte_t token_tag[TOKEN_TAG_BYTES_LEN];
+  iota_crypto_randombytes(token_tag, TOKEN_TAG_BYTES_LEN);
+
+  // create circulating and maximum supply
+  uint256_t* circ_supply = uint256_from_str("444444444");
+  uint256_t* max_supply = uint256_from_str("555555555");
+
+  // create metadata
+  byte_t test_data[] = "Test metadata...";
+  byte_buf_t* metadata = byte_buf_new_with_data(test_data, sizeof(test_data));
+
+  // create Feature Blocks
+  feat_blk_list_t* feat_blocks = new_feat_blk_list();
+  feat_blk_list_add_metadata(&feat_blocks, metadata->data, metadata->len);
+
+  // create Foundry Output
+  output_foundry_t* output = output_foundry_new(&addr, 123456789, native_tokens, 22, token_tag, circ_supply, max_supply,
+                                                SIMPLE_TOKEN_SCHEME, feat_blocks);
+  TEST_ASSERT_NOT_NULL(output);
+
+  // clean up
+  free(amount1);
+  free(amount2);
+  free(amount3);
+  free(circ_supply);
+  free(max_supply);
+  byte_buf_free(metadata);
+  native_tokens_free(&native_tokens);
+  free_feat_blk_list(feat_blocks);
+
+  return output;
+}
+
 static output_nft_t* create_output_nft() {
   // create random NFT address
   address_t addr = {};
@@ -185,12 +244,16 @@ void test_utxo_outputs() {
   output_alias_t* alias_output = create_output_alias();
   TEST_ASSERT_EQUAL_INT(0, utxo_outputs_add(&outputs, OUTPUT_ALIAS, alias_output));
 
+  // add foundry output to the output list
+  output_foundry_t* foundry_output = create_output_foundry();
+  TEST_ASSERT_EQUAL_INT(0, utxo_outputs_add(&outputs, OUTPUT_FOUNDRY, foundry_output));
+
   // add NFT output to the output list
   output_nft_t* nft_output = create_output_nft();
   TEST_ASSERT_EQUAL_INT(0, utxo_outputs_add(&outputs, OUTPUT_NFT, nft_output));
 
   // check outputs list length
-  TEST_ASSERT_EQUAL_INT(3, utxo_outputs_count(outputs));
+  TEST_ASSERT_EQUAL_INT(4, utxo_outputs_count(outputs));
 
   // check extended output
   utxo_output_t* output_from_list = utxo_outputs_get(outputs, 0);
@@ -227,8 +290,26 @@ void test_utxo_outputs() {
   TEST_ASSERT_EQUAL_UINT8(feat_blk_list_len(alias_output->feature_blocks),
                           feat_blk_list_len(alias_output_from_list->feature_blocks));
 
-  // check NFT output
+  // check foundry output
   output_from_list = utxo_outputs_get(outputs, 2);
+  TEST_ASSERT_EQUAL_INT(OUTPUT_FOUNDRY, output_from_list->output_type);
+  output_foundry_t* foundry_output_from_list = (output_foundry_t*)output_from_list->output;
+  TEST_ASSERT_EQUAL_UINT8(foundry_output->address->type, foundry_output_from_list->address->type);
+  TEST_ASSERT_EQUAL_MEMORY(foundry_output->address->address, foundry_output_from_list->address->address,
+                           ADDRESS_ED25519_BYTES);
+  TEST_ASSERT_EQUAL_UINT64(foundry_output->amount, foundry_output_from_list->amount);
+  TEST_ASSERT_EQUAL_UINT16(native_tokens_count(&foundry_output->native_tokens),
+                           native_tokens_count(&foundry_output_from_list->native_tokens));
+  TEST_ASSERT_EQUAL_INT32(foundry_output->serial, foundry_output_from_list->serial);
+  TEST_ASSERT_EQUAL_MEMORY(foundry_output->token_tag, foundry_output_from_list->token_tag, TOKEN_TAG_BYTES_LEN);
+  TEST_ASSERT_EQUAL_MEMORY(foundry_output->circ_supply, foundry_output_from_list->circ_supply, sizeof(uint256_t));
+  TEST_ASSERT_EQUAL_MEMORY(foundry_output->max_supply, foundry_output_from_list->max_supply, sizeof(uint256_t));
+  TEST_ASSERT_EQUAL_UINT8(foundry_output->token_scheme, foundry_output_from_list->token_scheme);
+  TEST_ASSERT_EQUAL_UINT8(feat_blk_list_len(foundry_output->feature_blocks),
+                          feat_blk_list_len(foundry_output_from_list->feature_blocks));
+
+  // check NFT output
+  output_from_list = utxo_outputs_get(outputs, 3);
   TEST_ASSERT_EQUAL_INT(OUTPUT_NFT, output_from_list->output_type);
   output_nft_t* nft_output_from_list = (output_nft_t*)output_from_list->output;
   TEST_ASSERT_EQUAL_UINT8(nft_output->address->type, nft_output_from_list->address->type);
@@ -257,6 +338,7 @@ void test_utxo_outputs() {
   // clean up
   free(extended_output);
   free(alias_output);
+  free(foundry_output);
   free(nft_output);
   free(outputs_list_buf);
   utxo_outputs_free(outputs);
