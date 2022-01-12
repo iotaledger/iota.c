d3e9177c237b1792c35eae01bd7e95002d60c0e2
Matjaz Verbole <matjaz.verbole@iota.org>
2022-01-12 09:26:15 +0100

    Implemented unit tests for different *_clone() functions

--- tests/core/test_feat_blocks.c
+++ tests/core/test_feat_blocks.c
@@ -615,6 +615,124 @@ void test_feat_block_list_sort() {
   free_feat_blk_list(blk_list);
 }
 
+void test_feat_block_list_clone() {
+  //=====NULL feature block list=====
+  feat_blk_list_t* new_blk_list = feat_blk_list_clone(NULL);
+  TEST_ASSERT_NULL(new_blk_list);
+
+  //=====Test feature block list object=====
+  feat_blk_list_t* blk_list = new_feat_blk_list();
+
+  // add a sender
+  address_t test_addr = {};
+  test_addr.type = ADDRESS_TYPE_ED25519;
+  iota_crypto_randombytes(test_addr.address, ADDRESS_ED25519_BYTES);
+  TEST_ASSERT(feat_blk_list_add_sender(&blk_list, &test_addr) == 0);
+  // add an issuer
+  test_addr.type = ADDRESS_TYPE_NFT;  // changed the type, but use the same address data
+  TEST_ASSERT(feat_blk_list_add_issuer(&blk_list, &test_addr) == 0);
+  // add a dust deposit return
+  uint64_t ddr_value = 1280097745;
+  TEST_ASSERT(feat_blk_list_add_ddr(&blk_list, ddr_value) == 0);
+  // add a timelock milestone index
+  uint32_t tmi_value = 12345678;
+  TEST_ASSERT(feat_blk_list_add_tmi(&blk_list, tmi_value) == 0);
+  // add a timelock Unix
+  uint32_t tu_value = 1639991908;
+  TEST_ASSERT(feat_blk_list_add_tu(&blk_list, tu_value) == 0);
+  // add a expiration milestone index
+  uint32_t emi_value = 145982608;
+  TEST_ASSERT(feat_blk_list_add_emi(&blk_list, emi_value) == 0);
+  // add a expiration Unix
+  uint32_t eu_value = 1839991908;
+  TEST_ASSERT(feat_blk_list_add_eu(&blk_list, eu_value) == 0);
+  // add a metadata
+  byte_t meta_data[256] = {};
+  iota_crypto_randombytes(meta_data, sizeof(meta_data));
+  TEST_ASSERT(feat_blk_list_add_metadata(&blk_list, meta_data, sizeof(meta_data)) == 0);
+  // add an indexaction tag
+  byte_t tag[MAX_INDEX_TAG_BYTES] = {};
+  iota_crypto_randombytes(tag, sizeof(tag));
+  TEST_ASSERT(feat_blk_list_add_indexaction(&blk_list, tag, sizeof(tag)) == 0);
+
+  // check length of the list
+  TEST_ASSERT(feat_blk_list_len(blk_list) == 9);
+
+  // print out the feature block list
+  feat_blk_list_print(blk_list);
+
+  // clone feature block list
+  new_blk_list = feat_blk_list_clone(blk_list);
+
+  // check new cloned feature block list
+  feat_block_t* tmp_blk = NULL;
+
+  // check length of the new feature block list
+  TEST_ASSERT(feat_blk_list_len(new_blk_list) == 9);
+
+  // 0: should be sender
+  tmp_blk = feat_blk_list_get(new_blk_list, 0);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_SENDER_BLOCK);
+  TEST_ASSERT(((address_t*)tmp_blk->block)->type == ADDRESS_TYPE_ED25519);
+  TEST_ASSERT_EQUAL_MEMORY(((address_t*)tmp_blk->block)->address, test_addr.address, ADDRESS_ED25519_BYTES);
+
+  // 1: should be issuer
+  tmp_blk = feat_blk_list_get(new_blk_list, 1);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_ISSUER_BLOCK);
+  TEST_ASSERT(((address_t*)tmp_blk->block)->type == ADDRESS_TYPE_NFT);
+  TEST_ASSERT_EQUAL_MEMORY(((address_t*)tmp_blk->block)->address, test_addr.address, ADDRESS_NFT_BYTES);
+
+  // 2: should be dust deposit return
+  tmp_blk = feat_blk_list_get(new_blk_list, 2);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_DUST_DEP_RET_BLOCK);
+  TEST_ASSERT(*(uint64_t*)tmp_blk->block == ddr_value);
+
+  // 3: should be timelock milestone index
+  tmp_blk = feat_blk_list_get(new_blk_list, 3);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_TIMELOCK_MS_INDEX_BLOCK);
+  TEST_ASSERT(*(uint32_t*)tmp_blk->block == tmi_value);
+
+  // 4: should be timelock Unix
+  tmp_blk = feat_blk_list_get(new_blk_list, 4);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_TIMELOCK_UNIX_BLOCK);
+  TEST_ASSERT(*(uint32_t*)tmp_blk->block == tu_value);
+
+  // 5: should be expiration milestone index
+  tmp_blk = feat_blk_list_get(new_blk_list, 5);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_EXPIRATION_MS_INDEX_BLOCK);
+  TEST_ASSERT(*(uint32_t*)tmp_blk->block == emi_value);
+
+  // 6: should be expiration Unix
+  tmp_blk = feat_blk_list_get(new_blk_list, 6);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_EXPIRATION_UNIX_BLOCK);
+  TEST_ASSERT(*(uint32_t*)tmp_blk->block == eu_value);
+
+  // 7: should be metadata
+  tmp_blk = feat_blk_list_get(new_blk_list, 7);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_METADATA_BLOCK);
+  TEST_ASSERT(((feat_metadata_blk_t*)tmp_blk->block)->data_len == sizeof(meta_data));
+  TEST_ASSERT_EQUAL_MEMORY(((feat_metadata_blk_t*)tmp_blk->block)->data, meta_data, sizeof(meta_data));
+
+  // 8: should be indexaction
+  tmp_blk = feat_blk_list_get(new_blk_list, 8);
+  TEST_ASSERT_NOT_NULL(tmp_blk);
+  TEST_ASSERT(tmp_blk->type == FEAT_INDEXATION_BLOCK);
+  TEST_ASSERT(((feat_indexaction_blk_t*)tmp_blk->block)->tag_len == sizeof(tag));
+  TEST_ASSERT_EQUAL_MEMORY(((feat_indexaction_blk_t*)tmp_blk->block)->tag, tag, sizeof(tag));
+
+  // clean up
+  free_feat_blk_list(new_blk_list);
+  free_feat_blk_list(blk_list);
+}
+
 int main() {
   UNITY_BEGIN();
 
@@ -629,6 +747,7 @@ int main() {
   RUN_TEST(test_indexaction);
   RUN_TEST(test_feat_block_list_append_all);
   RUN_TEST(test_feat_block_list_sort);
+  RUN_TEST(test_feat_block_list_clone);
 
   return UNITY_END();
 }
