8f396a35c76076e7c4ef7e9a5cd6c7157e6bf802
Sam Chen <sam.chen@iota.org>
2022-01-29 13:39:46 +0800

    Test - Added unlock conditions test case

--- tests/core/test_output_extended.c
+++ tests/core/test_output_extended.c
@@ -55,6 +55,8 @@ void setUp(void) {
   unlock_dust = cond_blk_dust_new(&test_addr, unlock_dust_amount);
   unlock_timelock = cond_blk_timelock_new(unlock_time_ms, unlock_time_unix);
   unlock_expir = cond_blk_expir_new(&test_addr, unlock_time_ms, unlock_time_unix);
+  unlock_state = cond_blk_state_new(&test_addr);
+  unlock_gov = cond_blk_governor_new(&test_addr);
 }
 
 void tearDown(void) {
@@ -66,6 +68,8 @@ void tearDown(void) {
   cond_blk_free(unlock_dust);
   cond_blk_free(unlock_timelock);
   cond_blk_free(unlock_expir);
+  cond_blk_free(unlock_state);
+  cond_blk_free(unlock_gov);
 }
 
 void test_output_extended() {
@@ -82,9 +86,9 @@ void test_output_extended() {
   iota_crypto_randombytes(addr.address, ADDRESS_ED25519_BYTES);
   // create Feature Blocks
   feat_blk_list_t* feat_blocks = feat_blk_list_new();
-  feat_blk_list_add_tag(&feat_blocks, test_tag, sizeof(test_tag));
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_metadata(&feat_blocks, test_meta, sizeof(test_meta));
+  TEST_ASSERT(feat_blk_list_add_tag(&feat_blocks, test_tag, sizeof(test_tag)) == 0);
+  TEST_ASSERT(feat_blk_list_add_sender(&feat_blocks, &addr) == 0);
+  TEST_ASSERT(feat_blk_list_add_metadata(&feat_blocks, test_meta, sizeof(test_meta)) == 0);
 
   // create Extended Output
   output_extended_t* output = output_extended_new(&addr, 123456789, native_tokens, unlock_conds, feat_blocks);
@@ -516,134 +520,74 @@ void test_output_extended_without_native_tokens_and_feature_blocks() {
   output_extended_free(deser_output);
 }
 
-void test_output_extended_syntactic() {
-#if 0
-  // create random ED25519 address
-  address_t addr = {};
-  addr.type = ADDRESS_TYPE_ED25519;
-  iota_crypto_randombytes(addr.address, ADDRESS_ED25519_BYTES);
+void test_output_extended_unlock_conditions() {
+  // create unlock conditions
+  cond_blk_list_t* unlock_conds = cond_blk_list_new();
 
-  // create Native Tokens
-  byte_t token_id1[NATIVE_TOKEN_ID_BYTES];
-  byte_t token_id2[NATIVE_TOKEN_ID_BYTES];
-  byte_t token_id3[NATIVE_TOKEN_ID_BYTES];
-  iota_crypto_randombytes(token_id1, NATIVE_TOKEN_ID_BYTES);
-  iota_crypto_randombytes(token_id2, NATIVE_TOKEN_ID_BYTES);
-  iota_crypto_randombytes(token_id3, NATIVE_TOKEN_ID_BYTES);
-  native_tokens_t* native_tokens = native_tokens_new();
-  uint256_t* amount1 = uint256_from_str("111111111");
-  native_tokens_add(&native_tokens, token_id1, amount1);
-  uint256_t* amount2 = uint256_from_str("222222222");
-  native_tokens_add(&native_tokens, token_id2, amount2);
-  uint256_t* amount3 = uint256_from_str("333333333");
-  native_tokens_add(&native_tokens, token_id3, amount3);
+  // invalid: empty unlock conditions
+  TEST_ASSERT_NULL(output_extended_new(&test_addr, 123456789, NULL, NULL, NULL));
+  TEST_ASSERT_NULL(output_extended_new(&test_addr, 123456789, NULL, unlock_conds, NULL));
 
-  // create Feature Blocks
-  feat_blk_list_t* feat_blocks = new_feat_blk_list();
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_ddr(&feat_blocks, 1000000);
+  // invalid unlock conditions: State Controller/Governanor
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_state) == 0);
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_gov) == 0);
+  TEST_ASSERT_NULL(output_extended_new(&test_addr, 123456789, NULL, unlock_conds, NULL));
+  cond_blk_list_free(unlock_conds);
 
-  //=====Test NULL address=====
-  output_extended_t* output = output_extended_new(&addr, 123456789, native_tokens, feat_blocks);
-  TEST_ASSERT_NOT_NULL(output);
-  output_extended_free(output);
-  output = output_extended_new(NULL, 123456789, native_tokens, feat_blocks);
-  TEST_ASSERT_NULL(output);
+  // invalid unlock condition: State Controller
+  unlock_conds = cond_blk_list_new();
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_state) == 0);
+  TEST_ASSERT_NULL(output_extended_new(&test_addr, 123456789, NULL, unlock_conds, NULL));
+  cond_blk_list_free(unlock_conds);
 
-  //=====Test minimum dust allowance=====
-  output = output_extended_new(&addr, 1000000, native_tokens, feat_blocks);
-  TEST_ASSERT_NOT_NULL(output);
-  output_extended_free(output);
-  output = output_extended_new(&addr, 999999, native_tokens, feat_blocks);
-  TEST_ASSERT_NULL(output);
-
-  //=====Test unsupported feature blocks type=====
-  output = output_extended_new(&addr, 123456789, native_tokens, feat_blocks);
-  TEST_ASSERT_NOT_NULL(output);
-  output_extended_free(output);
-  feat_blk_list_add_issuer(&feat_blocks, &addr);
-  output = output_extended_new(&addr, 123456789, native_tokens, feat_blocks);
-  TEST_ASSERT_NULL(output);
-  free_feat_blk_list(feat_blocks);
-
-  //=====Test maximum feature blocks count=====
-  feat_blocks = new_feat_blk_list();
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_ddr(&feat_blocks, 1000000);
-  output = output_extended_new(&addr, 123456789, native_tokens, feat_blocks);
-  TEST_ASSERT_NOT_NULL(output);
-  output_extended_free(output);
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_ddr(&feat_blocks, 1000000);
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_ddr(&feat_blocks, 1000000);
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_ddr(&feat_blocks, 1000000);
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  output = output_extended_new(&addr, 123456789, native_tokens, feat_blocks);
-  TEST_ASSERT_NULL(output);
-
-  // clean up
-  free(amount1);
-  free(amount2);
-  free(amount3);
-  native_tokens_free(&native_tokens);
-  free_feat_blk_list(feat_blocks);
-  output_extended_free(output);
-#endif
+  // invalid unlock condition: Governor
+  unlock_conds = cond_blk_list_new();
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_gov) == 0);
+  TEST_ASSERT_NULL(output_extended_new(&test_addr, 123456789, NULL, unlock_conds, NULL));
+  cond_blk_list_free(unlock_conds);
 }
 
 void test_output_extended_clone() {
-#if 0  // FIXME
   //=====NULL Extended Output object=====
   output_extended_t* new_output = output_extended_clone(NULL);
   TEST_ASSERT_NULL(new_output);
 
   //=====Test Extended Output object=====
-  // create random ED25519 address
-  address_t addr = {};
-  addr.type = ADDRESS_TYPE_ED25519;
-  iota_crypto_randombytes(addr.address, ADDRESS_ED25519_BYTES);
-
-  // create Native Tokens
-  byte_t token_id1[NATIVE_TOKEN_ID_BYTES] = {
-      0xDD, 0xA7, 0xC5, 0x79, 0x47, 0x9E, 0xC, 0x93, 0xCE, 0xA7, 0x93, 0x95, 0x41, 0xF8, 0x93, 0x4D, 0xF,  0x7E, 0x3A,
-      0x4,  0xCA, 0x52, 0xF8, 0x8B, 0x9B, 0x0, 0x25, 0xC0, 0xBE, 0x4A, 0xF6, 0x23, 0x59, 0x98, 0x6F, 0x64, 0xEF, 0x14};
-  byte_t token_id2[NATIVE_TOKEN_ID_BYTES] = {
-      0x74, 0x6B, 0xA0, 0xD9, 0x51, 0x41, 0xCB, 0x5B, 0x4B, 0xF7, 0x1C, 0x9D, 0x3E, 0x76, 0x81, 0xBE, 0xB6, 0xA3, 0xAE,
-      0x5A, 0x6D, 0x7C, 0x89, 0xD0, 0x98, 0x42, 0xDF, 0x86, 0x27, 0x5A, 0xF,  0x9,  0xCB, 0xE0, 0xF9, 0x1A, 0x6C, 0x6B};
-  byte_t token_id3[NATIVE_TOKEN_ID_BYTES] = {
-      0xBA, 0x26, 0x7E, 0x59, 0xE5, 0x31, 0x77, 0xB3, 0x2A, 0xA9, 0xBF, 0xE,  0x56, 0x31, 0x18, 0xC9, 0xE0, 0xAD, 0xD,
-      0x76, 0x88, 0x7B, 0x65, 0xFD, 0x58, 0x75, 0xB7, 0x13, 0x29, 0x73, 0x5B, 0x94, 0x2B, 0x81, 0x6A, 0x7F, 0xE6, 0x79};
-  native_tokens_t* native_tokens = native_tokens_new();
-  uint256_t* amount1 = uint256_from_str("111111111");
-  native_tokens_add(&native_tokens, token_id1, amount1);
-  uint256_t* amount2 = uint256_from_str("222222222");
-  native_tokens_add(&native_tokens, token_id2, amount2);
-  uint256_t* amount3 = uint256_from_str("333333333");
-  native_tokens_add(&native_tokens, token_id3, amount3);
+  // create unlock conditions
+  cond_blk_list_t* unlock_conds = cond_blk_list_new();
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_dust) == 0);
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_addr) == 0);
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_expir) == 0);
+  TEST_ASSERT(cond_blk_list_add(&unlock_conds, unlock_timelock) == 0);
 
   // create Feature Blocks
-  feat_blk_list_t* feat_blocks = new_feat_blk_list();
-  feat_blk_list_add_sender(&feat_blocks, &addr);
-  feat_blk_list_add_ddr(&feat_blocks, 1000000);
+  feat_blk_list_t* feat_blocks = feat_blk_list_new();
+  TEST_ASSERT(feat_blk_list_add_tag(&feat_blocks, test_tag, sizeof(test_tag)) == 0);
+  TEST_ASSERT(feat_blk_list_add_sender(&feat_blocks, &test_addr) == 0);
+  TEST_ASSERT(feat_blk_list_add_metadata(&feat_blocks, test_meta, sizeof(test_meta)) == 0);
 
   // create Extended Output
-  output_extended_t* output = output_extended_new(&addr, 123456789, native_tokens, feat_blocks);
+  output_extended_t* output = output_extended_new(&test_addr, 123456789, native_tokens, unlock_conds, feat_blocks);
   TEST_ASSERT_NOT_NULL(output);
 
   // clone Extended Output object
   new_output = output_extended_clone(output);
+  TEST_ASSERT_NOT_NULL(new_output);
 
-  // validate new Extended Output object
-  TEST_ASSERT_EQUAL_MEMORY(output->address, new_output->address, sizeof(address_t));
-  TEST_ASSERT_EQUAL_UINT64(output->amount, new_output->amount);
-
+  // validate Address
+  TEST_ASSERT_TRUE(address_equal(output->address, new_output->address));
+  // validate Amount
+  TEST_ASSERT(output->amount == new_output->amount);
   // validate Native Tokens
   TEST_ASSERT_NOT_NULL(output->native_tokens);
   TEST_ASSERT_NOT_NULL(new_output->native_tokens);
   TEST_ASSERT_EQUAL_UINT32(native_tokens_count(&output->native_tokens),
                            native_tokens_count(&new_output->native_tokens));
+  // validate Unlock Conditions
+  TEST_ASSERT_NOT_NULL(output->unlock_conditions);
+  TEST_ASSERT_NOT_NULL(new_output->unlock_conditions);
+  TEST_ASSERT_EQUAL_UINT8(cond_blk_list_len(output->unlock_conditions),
+                          cond_blk_list_len(new_output->unlock_conditions));
 
   // validate Feature Blocks
   TEST_ASSERT_NOT_NULL(output->feature_blocks);
@@ -654,14 +598,10 @@ void test_output_extended_clone() {
   output_extended_print(new_output, 0);
 
   // clean up
-  free(amount1);
-  free(amount2);
-  free(amount3);
-  native_tokens_free(&native_tokens);
-  free_feat_blk_list(feat_blocks);
+  cond_blk_list_free(unlock_conds);
+  feat_blk_list_free(feat_blocks);
   output_extended_free(new_output);
   output_extended_free(output);
-#endif
 }
 
 int main() {
@@ -671,8 +611,8 @@ int main() {
   RUN_TEST(test_output_extended_without_native_tokens);
   RUN_TEST(test_output_extended_without_feature_blocks);
   RUN_TEST(test_output_extended_without_native_tokens_and_feature_blocks);
-  RUN_TEST(test_output_extended_syntactic);
-  // RUN_TEST(test_output_extended_clone);
+  RUN_TEST(test_output_extended_unlock_conditions);
+  RUN_TEST(test_output_extended_clone);
 
   return UNITY_END();
 }
