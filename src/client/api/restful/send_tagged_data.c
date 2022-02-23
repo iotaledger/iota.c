e9ec52e40bd0267cd5dd534a9b3e18b5c1d1fbf7
rebinmannady93 <rabin.muhammed@iota.org>
2022-02-23 08:32:21 +0530

    Added test cases.
    Signed-off-by: rebinmannady93 <rabin.muhammed@iota.org>

--- src/client/api/restful/send_tagged_data.c
+++ src/client/api/restful/send_tagged_data.c
@@ -17,6 +17,17 @@ int send_tagged_data_message(iota_client_conf_t const* conf, byte_t tag[], uint8
     return -1;
   }
 
+  if (data_len > 0 && data == NULL) {
+    printf("[%s:%d] data cannot be null if data_len is greater than 0\n", __func__, __LINE__);
+    return -1;
+  }
+
+  // Max tag length allowed is 64
+  if (tag_len > 64) {
+    printf("[%s:%d] invalid tag\n", __func__, __LINE__);
+    return -1;
+  }
+
   res_tips_t* tips = NULL;
   if ((tips = res_tips_new()) == NULL) {
     printf("[%s:%d] allocate tips response failed\n", __func__, __LINE__);
@@ -83,19 +94,86 @@ int send_tagged_data_message(iota_client_conf_t const* conf, byte_t tag[], uint8
 
   // create payload object
   if ((payload = cJSON_CreateObject()) == NULL) {
-    printf("[%s:%d] creating message object failed\n", __func__, __LINE__);
+    printf("[%s:%d] creating payload object failed\n", __func__, __LINE__);
     goto end;
   }
 
-  // Add type to payload
-  if (!cJSON_AddNumberToObject(msg_obj, JSON_KEY_TYPE, CORE_MESSAGE_PAYLOAD_TAGGED)) {
+  // add type to payload
+  if (!cJSON_AddNumberToObject(payload, JSON_KEY_TYPE, CORE_MESSAGE_PAYLOAD_TAGGED)) {
     printf("[%s:%d] adding type to payload failed\n", __func__, __LINE__);
     goto end;
   }
 
-  printf("%s\n", cJSON_Print(msg_obj));
+  // add tag
+  // max tag length is 64
+  char tag_str[BIN_TO_HEX_STR_BYTES(64)] = {0};
+  if (bin_2_hex(tag, tag_len, tag_str, sizeof(tag_str)) != 0) {
+    printf("[%s:%d] bin to hex tag conversion failed\n", __func__, __LINE__);
+    goto end;
+  }
+  if (!cJSON_AddStringToObject(payload, JSON_KEY_TAG, tag_str)) {
+    printf("[%s:%d] adding tag to payload failed\n", __func__, __LINE__);
+    goto end;
+  }
 
-  ret = 0;
+  // data
+  if (data) {
+    char* data_str = malloc(BIN_TO_HEX_STR_BYTES(data_len));
+    if (!data_str) {
+      printf("[%s:%d] OOM\n", __func__, __LINE__);
+      goto end;
+    }
+    if (bin_2_hex(data, data_len, data_str, BIN_TO_HEX_STR_BYTES(data_len)) != 0) {
+      printf("[%s:%d] bin to hex data conversion failed\n", __func__, __LINE__);
+      free(data_str);
+      goto end;
+    }
+    if (!cJSON_AddStringToObject(payload, JSON_KEY_DATA, data_str)) {
+      printf("[%s:%d] adding tag data failed\n", __func__, __LINE__);
+      free(data_str);
+      goto end;
+    }
+    free(data_str);
+  } else {
+    // add a null data to tagged data
+    if (!cJSON_AddNullToObject(payload, JSON_KEY_DATA)) {
+      printf("[%s:%d] adding null data payload failed\n", __func__, __LINE__);
+      goto end;
+    }
+  }
+
+  // add payload to message
+  if (!cJSON_AddItemToObject(msg_obj, JSON_KEY_PAYLOAD, payload)) {
+    printf("[%s:%d] adding payload failed\n", __func__, __LINE__);
+    goto end;
+  }
+
+  // json object to json string
+  char* msg_str = NULL;
+  if ((msg_str = cJSON_PrintUnformatted(msg_obj)) == NULL) {
+    printf("[%s:%d] converting json to string failed\n", __func__, __LINE__);
+    goto end;
+  }
+
+  // put json string into byte_buf_t
+  byte_buf_t* json_data = byte_buf_new();
+  json_data->data = (byte_t*)msg_str;
+  json_data->cap = json_data->len = strlen(msg_str) + 1;
+
+  // config http client
+  http_client_config_t http_conf = {
+      .host = conf->host, .path = "/api/v2/messages", .use_tls = conf->use_tls, .port = conf->port};
+  long http_st_code = 0;
+  byte_buf_t* http_res = byte_buf_new();
+  if ((ret = http_client_post(&http_conf, json_data, http_res, &http_st_code)) == 0) {
+    // deserialize node response
+    byte_buf2str(http_res);
+    ret = deser_send_message_response((char const*)http_res->data, res);
+  } else {
+    printf("[%s:%d]: http client post failed\n", __func__, __LINE__);
+  }
+  byte_buf_free(json_data);
+  byte_buf_free(http_res);
 
 end:
   cJSON_Delete(msg_obj);
--- tests/client/api_restful/test_send_tagged_data.c
+++ tests/client/api_restful/test_send_tagged_data.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <unity/unity.h>
 
+#include "client/api/json_parser/json_keys.h"
 #include "client/api/restful/send_tagged_data.h"
+#include "client/network/http.h"
 #include "core/utils/byte_buffer.h"
 #include "core/utils/macros.h"
 #include "crypto/iota_crypto.h"
@@ -14,29 +16,135 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-#define TAG_LEN 14
+#define TAG_LEN 15
 #define TAG_INVALID_LEN 70
 #define TAG_DATA_LEN 64
 
 char const* const tag = "IOTA TAGGED DATA";
 char const* const tag_invalid_len = "IOTA TAGGED DATA, IOTA TAGGED DATA, IOTA TAGGED DATA, IOTA TAGGED DATA";
+byte_t binary_tag[TAG_LEN] = {0x13, 0x94, 0x12, 0xdd, 0x2b, 0xff, 0xd4, 0x55, 0x62, 0x90, 0xfd, 0x6f, 0xa8, 0x30, 0x1f};
 
 void test_send_tagged_data() {
   iota_client_conf_t ctx = {.host = TEST_NODE_HOST, .port = TEST_NODE_PORT, .use_tls = TEST_IS_HTTPS};
 
   byte_t tag_data[TAG_DATA_LEN];
   iota_crypto_randombytes(tag_data, TAG_DATA_LEN);
-
   res_send_message_t res = {};
   res.is_error = false;
 
+  // invalid parameters
+  TEST_ASSERT(send_tagged_data_message(NULL, (byte_t*)tag, TAG_LEN, tag_data, TAG_DATA_LEN, &res) == -1);
+  TEST_ASSERT(send_tagged_data_message(&ctx, NULL, TAG_LEN, tag_data, TAG_DATA_LEN, &res) == -1);
+  TEST_ASSERT(send_tagged_data_message(&ctx, (byte_t*)tag, TAG_LEN, tag_data, TAG_DATA_LEN, NULL) == -1);
+  TEST_ASSERT(send_tagged_data_message(&ctx, (byte_t*)tag, TAG_LEN, NULL, TAG_DATA_LEN, &res) == -1);
+  TEST_ASSERT(send_tagged_data_message(&ctx, (byte_t*)tag_invalid_len, TAG_INVALID_LEN, NULL, TAG_DATA_LEN, &res) ==
+              -1);
+
+  // Valid data and tag
   TEST_ASSERT(send_tagged_data_message(&ctx, (byte_t*)tag, TAG_LEN, tag_data, TAG_DATA_LEN, &res) == 0);
+
+  // Get message by message id and verify tag and data
+  char cmd_str[82] = {0};  // "/api/v2/messages/{messageid}"
+  snprintf(cmd_str, 82, "/api/v2/messages/%s", res.u.msg_id);
+
+  // http client configuration
+  http_client_config_t http_conf = {.host = ctx.host, .path = cmd_str, .use_tls = ctx.use_tls, .port = ctx.port};
+
+  byte_buf_t* http_res = byte_buf_new();
+  TEST_ASSERT_NOT_NULL(http_res);
+
+  // send request via http client
+  int ret = -1;
+  long st = 0;
+  TEST_ASSERT(http_client_get(&http_conf, http_res, &st) == 0);
+  byte_buf2str(http_res);
+
+  cJSON* json_obj = cJSON_Parse((char const* const)http_res->data);
+  TEST_ASSERT_NOT_NULL(json_obj);
+  cJSON* payload = cJSON_GetObjectItemCaseSensitive(json_obj, JSON_KEY_PAYLOAD);
+  TEST_ASSERT_NOT_NULL(payload);
+  // tag
+  cJSON* json_tag = cJSON_GetObjectItemCaseSensitive(payload, JSON_KEY_TAG);
+  TEST_ASSERT_NOT_NULL(json_tag);
+  // data
+  cJSON* json_data = cJSON_GetObjectItemCaseSensitive(payload, JSON_KEY_DATA);
+  TEST_ASSERT_NOT_NULL(json_data);
+
+  char tag_hex[TAG_LEN * 2 + 1] = {0};
+  TEST_ASSERT(bin_2_hex((byte_t*)tag, TAG_LEN, tag_hex, sizeof(tag_hex)) == 0);
+
+  // check if tag is matching
+  TEST_ASSERT_EQUAL_MEMORY(tag_hex, json_tag->valuestring, TAG_LEN);
+
+  char data_hex[TAG_DATA_LEN * 2 + 1] = {0};
+  TEST_ASSERT(bin_2_hex(tag_data, TAG_DATA_LEN, data_hex, sizeof(data_hex)) == 0);
+
+  // check if data is matching
+  TEST_ASSERT_EQUAL_MEMORY(data_hex, json_data->valuestring, TAG_DATA_LEN);
+
+  cJSON_Delete(json_obj);
+  byte_buf_free(http_res);
+}
+
+void test_send_binary_tagged_data() {
+  iota_client_conf_t ctx = {.host = TEST_NODE_HOST, .port = TEST_NODE_PORT, .use_tls = TEST_IS_HTTPS};
+
+  byte_t tag_data[TAG_DATA_LEN];
+  iota_crypto_randombytes(tag_data, TAG_DATA_LEN);
+  res_send_message_t res = {};
+  res.is_error = false;
+
+  // Valid data and tag
+  TEST_ASSERT(send_tagged_data_message(&ctx, binary_tag, TAG_LEN, tag_data, TAG_DATA_LEN, &res) == 0);
+
+  // Get message by message id and verify tag and data
+  char cmd_str[82] = {0};  // "/api/v2/messages/{messageid}"
+  snprintf(cmd_str, 82, "/api/v2/messages/%s", res.u.msg_id);
+
+  // http client configuration
+  http_client_config_t http_conf = {.host = ctx.host, .path = cmd_str, .use_tls = ctx.use_tls, .port = ctx.port};
+
+  byte_buf_t* http_res = byte_buf_new();
+  TEST_ASSERT_NOT_NULL(http_res);
+
+  // send request via http client
+  int ret = -1;
+  long st = 0;
+  TEST_ASSERT(http_client_get(&http_conf, http_res, &st) == 0);
+  byte_buf2str(http_res);
+
+  cJSON* json_obj = cJSON_Parse((char const* const)http_res->data);
+  TEST_ASSERT_NOT_NULL(json_obj);
+  cJSON* payload = cJSON_GetObjectItemCaseSensitive(json_obj, JSON_KEY_PAYLOAD);
+  TEST_ASSERT_NOT_NULL(payload);
+  // tag
+  cJSON* json_tag = cJSON_GetObjectItemCaseSensitive(payload, JSON_KEY_TAG);
+  TEST_ASSERT_NOT_NULL(json_tag);
+  // data
+  cJSON* json_data = cJSON_GetObjectItemCaseSensitive(payload, JSON_KEY_DATA);
+  TEST_ASSERT_NOT_NULL(json_data);
+
+  char tag_hex[TAG_LEN * 2 + 1] = {0};
+  TEST_ASSERT(bin_2_hex(binary_tag, TAG_LEN, tag_hex, sizeof(tag_hex)) == 0);
+
+  // check if tag is matching
+  TEST_ASSERT_EQUAL_MEMORY(tag_hex, json_tag->valuestring, TAG_LEN);
+
+  char data_hex[TAG_DATA_LEN * 2 + 1] = {0};
+  TEST_ASSERT(bin_2_hex(tag_data, TAG_DATA_LEN, data_hex, sizeof(data_hex)) == 0);
+
+  // check if data is matching
+  TEST_ASSERT_EQUAL_MEMORY(data_hex, json_data->valuestring, TAG_DATA_LEN);
+
+  cJSON_Delete(json_obj);
+  byte_buf_free(http_res);
 }
 
 int main() {
   UNITY_BEGIN();
 #if TEST_TANGLE_ENABLE
   RUN_TEST(test_send_tagged_data);
+  RUN_TEST(test_send_binary_tagged_data);
 #endif
   return UNITY_END();
 }
