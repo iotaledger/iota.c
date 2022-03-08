4919d5fbd49c576e7540230016f26834ab723a3d
rebinmannady93 <rabin.muhammed@iota.org>
2022-03-08 11:37:06 +0530

    Updated test vectors with the same validity as in test_vectors.json
    Signed-off-by: rebinmannady93 <rabin.muhammed@iota.org>

--- tests/core/test_vectors.h
+++ tests/core/test_vectors.h
@@ -47,19 +47,19 @@ static test_vectors_t test_vectors[] = {{"008f5a6fdcfef8989fb88312cbc956ccc54dce
                                          "cdb267ce40c5cd45306fa5d2f29731459387dbf9eb933b7bd5aed9a765b88d4d",
                                          "160a1cb0dc9c0258cd0a7d23e94d8fa878bcb1925f2c64246b2dee1796bed5125ec6bc982a269"
                                          "b723e0668e540911a9a6a58921d6925e434ab10aa7940551a09",
-                                         false},
+                                         true},
                                         {"001482f60b75cc1a6b1fe9810b64b95a978c35ed73ff371cb99f6f1fd0479d7b21",
                                          "e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec4011eaccd55b53f56c",
                                          "cdb267ce40c5cd45306fa5d2f29731459387dbf9eb933b7bd5aed9a765b88d4d",
                                          "21122a84e0b5fca4052f5b1235c80a537878b38f3142356b2c2384ebad4668b7e40bc836dac0f"
                                          "71076f9abe3a53f9c03c1ceeeddb658d0030494ace586687405",
-                                         false},
+                                         true},
                                         {"002aeb2a345ceecd740ffa7cd891ac0116dd10fe169079d158e492043ec49b834d",
                                          "85e241a07d148b41e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec40",
                                          "442aad9f089ad9e14647b1ef9099a1ff4798d78589e66f28eca69c11f582a623",
                                          "e96f66be976d82e60150baecff9906684aebb1ef181f67a7189ac78ea23b6c0e547f7690a0e2d"
                                          "dcd04d87dbc3490dc19b3b3052f7ff0538cb68afb369ba3a514",
-                                         true},
+                                         false},
                                         {"002aeb2a345ceecd740ffa7cd891ac0116dd10fe169079d158e492043ec49b834d",
                                          "85e241a07d148b41e47d62c63f830dc7a6851a0b1f33ae4bb2f507fb6cffec40",
                                          "442aad9f089ad9e14647b1ef9099a1ff4798d78589e66f28eca69c11f582a623",
@@ -77,7 +77,7 @@ static test_vectors_t test_vectors[] = {{"008f5a6fdcfef8989fb88312cbc956ccc54dce
                                          "f7badec5b8abeaf699583992219b7b223f1df3fbbea919844e3f7c554a43dd43",
                                          "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffca8c5b64cd208"
                                          "982aa38d4936621a4775aa233aa0505711d8fdcfdaa943d4908",
-                                         false},
+                                         true},
                                         {"00f25275baf4e267a1ec213dfd8283e9c97bf8c3494752c1c14b4bd5e5f9bfa9d8",
                                          "e96b7021eb39c1a163b6da4e3093dcd3f21387da4cc4572be588fafae23c155b",
                                          "ecffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
