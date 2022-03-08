380137b40ad663f17c7c3c75683db6fd91160a34
Matjaz Verbole <matjaz.verbole@iota.org>
2022-03-08 11:33:56 +0100

    Split unit tests for storage deposit and byte cost config modules

--- tests/core/test_storage_deposit.c
+++ tests/core/test_storage_deposit.c
@@ -11,34 +11,6 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_storage_deposit_create_new_config() {
-  uint16_t byte_cost = (uint16_t)123456;
-  uint8_t byte_factor_data = 111;
-  uint8_t byte_factor_key = 222;
-  uint16_t byte_offset = (IOTA_OUTPUT_ID_BYTES * byte_factor_key) + (IOTA_MESSAGE_ID_BYTES * byte_factor_data) +
-                         (sizeof(uint32_t) * byte_factor_data) + (sizeof(uint32_t) * byte_factor_data);
-
-  byte_cost_config_t* config = byte_cost_config_new(byte_cost, byte_factor_data, byte_factor_key);
-
-  TEST_ASSERT_EQUAL_UINT16(byte_cost, config->v_byte_cost);
-  TEST_ASSERT_EQUAL_UINT8(byte_factor_data, config->v_byte_factor_data);
-  TEST_ASSERT_EQUAL_UINT8(byte_factor_key, config->v_byte_factor_key);
-  TEST_ASSERT_EQUAL_UINT16(byte_offset, config->v_byte_offset);
-
-  byte_cost_config_free(config);
-}
-
-void test_storage_deposit_create_new_default_config() {
-  byte_cost_config_t* config = byte_cost_config_default_new();
-
-  TEST_ASSERT_EQUAL_UINT16(500, config->v_byte_cost);
-  TEST_ASSERT_EQUAL_UINT8(1, config->v_byte_factor_data);
-  TEST_ASSERT_EQUAL_UINT8(10, config->v_byte_factor_key);
-  TEST_ASSERT_EQUAL_UINT16(380, config->v_byte_offset);
-
-  byte_cost_config_free(config);
-}
-
 void test_storage_deposit_check_sufficient_output_deposit_null_parameters() {
   byte_cost_config_t config;
   bool result = storage_deposit_sufficient_output_deposit_check(&config, OUTPUT_BASIC, NULL);
@@ -213,8 +185,6 @@ void test_storage_deposit_check_sufficient_output_not_microtransaction() {
 int main() {
   UNITY_BEGIN();
 
-  RUN_TEST(test_storage_deposit_create_new_config);
-  RUN_TEST(test_storage_deposit_create_new_default_config);
   RUN_TEST(test_storage_deposit_check_sufficient_output_deposit_null_parameters);
   RUN_TEST(test_storage_deposit_check_sufficient_output_deposit_unsupported_type);
   RUN_TEST(test_storage_deposit_check_sufficient_output_deposit);
