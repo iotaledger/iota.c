a68000ef0140d563878fcfdca096620240f5c643
Sam Chen <sam.chen@iota.org>
2022-03-11 14:48:18 +0800

    all valid elements should fulfil storage protection

--- src/core/models/outputs/outputs.c
+++ src/core/models/outputs/outputs.c
@@ -386,8 +386,11 @@ bool utxo_outputs_syntactic(utxo_outputs_list_t *outputs, byte_cost_config_t *by
       return is_valid;
     } else {
       // amount must fulfill the storage protection and must not be zero
-      return storage_deposit_check(byte_cost, elm->output->output_type, elm->output->output);
+      is_valid = storage_deposit_check(byte_cost, elm->output->output_type, elm->output->output);
+      if (is_valid == false) {
+        return is_valid;
+      }
     }
   }
-  return false;
+  return true;
 }
