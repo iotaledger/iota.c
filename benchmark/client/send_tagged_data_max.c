4cb937b7e5aa327885eac1d24487628d62cd6297
Matjaz Verbole <matjaz.verbole@iota.org>
2022-04-07 09:29:43 +0200

    Created benchmark application for send tagged data with maximum length of tag and data

---
+++ benchmark/client/send_tagged_data_max.c
@@ -0,0 +1,54 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdio.h>
+
+#include "client/api/restful/get_node_info.h"
+#include "client/api/restful/send_tagged_data.h"
+#include "core/models/outputs/feat_blocks.h"
+
+#define NODE_HOST "api.alphanet.iotaledger.net"
+#define NODE_PORT 443
+#define IS_HTTPS true
+
+int main() {
+  iota_client_conf_t ctx = {.host = NODE_HOST, .port = NODE_PORT, .use_tls = IS_HTTPS};
+
+  res_node_info_t* info = res_node_info_new();
+  if (!info) {
+    printf("[%s:%d]: Can not create node info object!\n", __func__, __LINE__);
+    return -1;
+  }
+
+  int result = get_node_info(&ctx, info);
+  if (result != 0 || info->is_error || info->u.output_node_info == NULL) {
+    printf("[%s:%d]: Can not received node info data!\n", __func__, __LINE__);
+    res_node_info_free(info);
+    return -1;
+  }
+
+  byte_t tag[MAX_INDEX_TAG_BYTES] = "Test tagged data from a benchmark application. Test tagged dat";
+  byte_t* tag_data = malloc(MAX_METADATA_LENGTH_BYTES);
+  if (!tag_data) {
+    printf("[%s:%d]: Can not create tag data object!\n", __func__, __LINE__);
+    res_node_info_free(info);
+    return -1;
+  }
+  res_send_message_t res = {};
+
+  result = send_tagged_data_message(&ctx, info->u.output_node_info->protocol_version, tag, sizeof(tag), tag_data,
+                                    MAX_METADATA_LENGTH_BYTES, &res);
+  if (result != 0 || res.is_error) {
+    printf("[%s:%d]: Can not send tagged data message!\n", __func__, __LINE__);
+    free(tag_data);
+    res_node_info_free(info);
+    return -1;
+  }
+  free(tag_data);
+  res_node_info_free(info);
+
+  printf("[%s:%d]: Message successfully send! URL: https://%s/api/v2/messages/0x%s\n", __func__, __LINE__, NODE_HOST,
+         res.u.msg_id);
+
+  return 0;
+}
