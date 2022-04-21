74271417d62e4995f202417f94530eec13fe6ba1
rebinmannady93 <rabin.muhammed@iota.org>
2022-04-21 10:58:48 +0530

    Added output payload events.
    Signed-off-by: rebinmannady93 <rabin.muhammed@iota.org>

--- src/client/api/events/sub_outputs_payload.c
+++ src/client/api/events/sub_outputs_payload.c
@@ -4,108 +4,122 @@
 #include "client/api/events/sub_outputs_payload.h"
 #include "client/api/json_parser/json_utils.h"
 #include "core/address.h"
-
-int event_parse_outputs_payload(char const data[], event_outputs_payload_t *res) {
-  int ret = -1;
-  cJSON *json_obj = cJSON_Parse(data);
-  if (json_obj == NULL) {
-    printf("[%s:%d] OOM\n", __func__, __LINE__);
-    return ret;
-  }
-
-  // message ID
-  if ((ret = json_get_string(json_obj, JSON_KEY_MSG_ID, res->msg_id, sizeof(res->msg_id))) != 0) {
-    printf("[%s:%d]: parsing %s failed\n", __func__, __LINE__, JSON_KEY_MSG_ID);
-    goto end;
-  }
-
-  // transaction ID
-  if ((ret = json_get_string(json_obj, JSON_KEY_TX_ID, res->tx_id, sizeof(res->tx_id))) != 0) {
-    printf("[%s:%d]: parsing %s failed\n", __func__, __LINE__, JSON_KEY_TX_ID);
-    goto end;
-  }
-
-  // output index
-  if ((ret = json_get_uint16(json_obj, JSON_KEY_OUTPUT_IDX, &res->output_index)) != 0) {
-    printf("[%s:%d]: gets %s json uint16 failed\n", __func__, __LINE__, JSON_KEY_OUTPUT_IDX);
-    goto end;
-  }
-
-  // is spent
-  if ((ret = json_get_boolean(json_obj, JSON_KEY_IS_SPENT, &res->is_spent)) != 0) {
-    printf("[%s:%d]: gets %s json bool failed\n", __func__, __LINE__, JSON_KEY_IS_SPENT);
-    goto end;
-  }
-
-  // ledgerIndex
-  if ((ret = json_get_uint32(json_obj, JSON_KEY_LEDGER_IDX, &res->ledger_index)) != 0) {
-    printf("[%s:%d]: gets %s json uint64 failed\n", __func__, __LINE__, JSON_KEY_LEDGER_IDX);
-    goto end;
-  }
-
-  // output object
-  cJSON *j_output = cJSON_GetObjectItemCaseSensitive(json_obj, JSON_KEY_OUTPUT);
-  if (j_output) {
-    if ((ret = json_get_uint32(j_output, JSON_KEY_TYPE, &res->output.output_type)) != 0) {
-      printf("[%s:%d]: gets %s json uint32 failed\n", __func__, __LINE__, JSON_KEY_TYPE);
-      goto end;
-    }
-
-    // amount
-    if ((ret = json_get_uint64(j_output, JSON_KEY_AMOUNT, &res->output.amount)) != 0) {
-      printf("[%s:%d]: gets %s json uint64 failed\n", __func__, __LINE__, JSON_KEY_AMOUNT);
-      goto end;
-    }
-
-    // address object
-    cJSON *j_addr = cJSON_GetObjectItemCaseSensitive(j_output, JSON_KEY_ADDR);
-    if (j_addr) {
-      // ed25519 address
-      if ((ret = json_get_string(j_addr, JSON_KEY_ADDR, res->output.addr, sizeof(res->output.addr))) != 0) {
-        printf("[%s:%d]: parsing %s failed\n", __func__, __LINE__, JSON_KEY_ADDR);
-        goto end;
-      }
-    } else {
-      printf("[%s:%d]: address object not found\n", __func__, __LINE__);
-      ret = -1;
-      goto end;
-    }
-  } else {
-    printf("[%s:%d]: output object not found\n", __func__, __LINE__);
-    ret = -1;
-    goto end;
-  }
-
-end:
-
-  cJSON_Delete(json_obj);
-  return ret;
-}
-
-int event_sub_address_outputs(event_client_handle_t client, int *mid, char const addr[], bool is_bech32, int qos) {
-  if ((strlen(addr)) != IOTA_ADDRESS_HEX_BYTES) {
-    printf("[%s:%d]: Address length is invalid\n", __func__, __LINE__);
-    return -1;
-  }
-  // 91 is the max length of string addresses/ed25519/[addr_str]/outputs
-  char topic_buff[91] = {};
-  if (is_bech32) {
-    sprintf(topic_buff, "addresses/%s/outputs", addr);
-  } else {
-    sprintf(topic_buff, "addresses/ed25519/%s/outputs", addr);
-  }
-  return event_subscribe(client, mid, topic_buff, qos);
-}
+#include "core/models/outputs/output_foundry.h"
+#include "core/utils/iota_str.h"
+#include "core/utils/macros.h"
 
 int event_sub_outputs_id(event_client_handle_t client, int *mid, char const output_id[], int qos) {
-  if ((strlen(output_id)) != OUTPUT_ID_LEN) {
+  if ((strlen(output_id)) != BIN_TO_HEX_BYTES(IOTA_OUTPUT_ID_BYTES)) {
     printf("[%s:%d]: Output Id length is invalid\n", __func__, __LINE__);
     return -1;
   }
 
   // Buffer enough for outputs/{outputId}
-  char topic_buff[77] = {};
-  sprintf(topic_buff, "outputs/%s", output_id);
+  char topic_buff[79] = {};
+  sprintf(topic_buff, "outputs/0x%s", output_id);
+
+  return event_subscribe(client, mid, topic_buff, qos);
+}
+
+int event_sub_outputs_unlock_address(event_client_handle_t client, int *mid, char const *const unlock_condition,
+                                     char const *const addr_bech32, int qos) {
+  // Check if addr_bech32 has minimum required length
+  if (strlen(addr_bech32) < BIN_TO_HEX_STR_BYTES(ADDRESS_MIN_BYTES)) {
+    printf("[%s:%d] invalid bech32 address\n", __func__, __LINE__);
+    return -1;
+  }
+  // Check if unlock condition is present
+  if (strlen(unlock_condition) > 0) {
+    printf("[%s:%d] invalid unlock condition\n", __func__, __LINE__);
+    return -1;
+  }
+
+  iota_str_t *topic_buff = NULL;
+  char const *const topic_str = "outputs/unlock/";
+
+  topic_buff =
+      iota_str_reserve(strlen(topic_str) + strlen(unlock_condition) + BIN_TO_HEX_STR_BYTES(ADDRESS_MAX_BYTES) + 2);
+  if (topic_buff == NULL) {
+    printf("[%s:%d]: allocate command buffer failed\n", __func__, __LINE__);
+    return -1;
+  }
+
+  // composing API command
+  snprintf(topic_buff->buf, topic_buff->cap, "%s%s%s%s", topic_str, unlock_condition, "/", addr_bech32);
+  topic_buff->len = strlen(topic_buff->buf);
+
+  int ret = event_subscribe(client, mid, topic_buff->buf, qos);
+  iota_str_destroy(topic_buff);
+  return ret;
+}
+
+int event_sub_outputs_unlock_address_spent(event_client_handle_t client, int *mid, char const *const unlock_condition,
+                                           char const *const addr_bech32, int qos) {
+  // Check if addr_bech32 has minimum required length
+  if (strlen(addr_bech32) < BIN_TO_HEX_STR_BYTES(ADDRESS_MIN_BYTES)) {
+    printf("[%s:%d] invalid bech32 address\n", __func__, __LINE__);
+    return -1;
+  }
+  // Check if unlock condition is present
+  if (strlen(unlock_condition) > 0) {
+    printf("[%s:%d] invalid unlock condition\n", __func__, __LINE__);
+    return -1;
+  }
+
+  iota_str_t *topic_buff = NULL;
+  char const *const topic_str = "outputs/unlock/";
+  // outputs/unlock/{condition}/{address}/spent
+  topic_buff =
+      iota_str_reserve(strlen(topic_str) + strlen(unlock_condition) + BIN_TO_HEX_STR_BYTES(ADDRESS_MAX_BYTES) + 8);
+  if (topic_buff == NULL) {
+    printf("[%s:%d]: allocate command buffer failed\n", __func__, __LINE__);
+    return -1;
+  }
+
+  // composing API command
+  snprintf(topic_buff->buf, topic_buff->cap, "%s%s%s%s%s", topic_str, unlock_condition, "/", addr_bech32, "/spent");
+  topic_buff->len = strlen(topic_buff->buf);
+
+  int ret = event_subscribe(client, mid, topic_buff->buf, qos);
+  iota_str_destroy(topic_buff);
+  return ret;
+}
+
+int event_sub_outputs_alias_id(event_client_handle_t client, int *mid, char const alias_id[], int qos) {
+  if (strlen(alias_id) != BIN_TO_HEX_BYTES(ALIAS_ID_BYTES)) {
+    printf("[%s:%d] incorrect length of id\n", __func__, __LINE__);
+    return -1;
+  }
+
+  // Buffer enough for outputs/aliases/{aliasId}
+  char topic_buff[59] = {};
+  sprintf(topic_buff, "outputs/aliases/0x%s", alias_id);
+
+  return event_subscribe(client, mid, topic_buff, qos);
+}
+
+int event_sub_outputs_nft_id(event_client_handle_t client, int *mid, char const nft_id[], int qos) {
+  if (strlen(nft_id) != BIN_TO_HEX_BYTES(NFT_ID_BYTES)) {
+    printf("[%s:%d] incorrect length of id\n", __func__, __LINE__);
+    return -1;
+  }
+
+  // Buffer enough for outputs/nfts/{nftId}
+  char topic_buff[56] = {};
+  sprintf(topic_buff, "outputs/nfts/0x%s", nft_id);
+
+  return event_subscribe(client, mid, topic_buff, qos);
+}
+
+int event_sub_outputs_foundry_id(event_client_handle_t client, int *mid, char const foundry_id[], int qos) {
+  if (strlen(foundry_id) != BIN_TO_HEX_BYTES(FOUNDRY_ID_BYTES)) {
+    printf("[%s:%d] incorrect length of id\n", __func__, __LINE__);
+    return -1;
+  }
+
+  // Buffer enough for outputs/foundries/{foundryId}
+  char topic_buff[73] = {};
+  sprintf(topic_buff, "outputs/foundries/0x%s", foundry_id);
 
   return event_subscribe(client, mid, topic_buff, qos);
 }
