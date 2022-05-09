9265f77b11f77850e9cc53d84178565461c2e26d
rebinmannady93 <rabin.muhammed@iota.org>
2022-05-09 19:29:21 +0530

    Moved back event topics from constants.h to node_event.h
    Signed-off-by: rebinmannady93 <rabin.muhammed@iota.org>

--- src/client/constants.h
+++ src/client/constants.h
@@ -10,15 +10,4 @@
 
 #define IOTA_ENDPOINT_MAX_LEN 256
 
-/***** Defines related to Node Events *****/
-#define TOPIC_MILESTONE_LATEST "milestone-info/latest"
-#define TOPIC_MILESTONE_CONFIRMED "milestone-info/confirmed"
-#define TOPIC_MILESTONES "milestones"
-#define TOPIC_MS_REFERENCED "message-metadata/referenced"
-#define TOPIC_MESSAGES "messages"
-#define TOPIC_MS_TRANSACTION "messages/transaction"
-#define TOPIC_MS_TXN_TAGGED_DATA "messages/transaction/tagged-data"
-#define TOPIC_MS_MILESTONE "messages/milestone"
-#define TOPIC_MS_TAGGED_DATA "messages/tagged-data"
-
 #endif
