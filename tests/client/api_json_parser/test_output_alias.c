0f1e9f12aa5fdb5ad04901f323af210ea5123763
Matjaz Verbole <matjaz.verbole@iota.org>
2022-05-18 11:44:01 +0200

    Fixed unit tests regarding alias id

--- tests/client/api_json_parser/test_output_alias.c
+++ tests/client/api_json_parser/test_output_alias.c
@@ -14,10 +14,10 @@ void tearDown(void) {}
 
 void test_parse_alias_output_basic() {
   char const *const json_res =
-      "{\"type\":4, \"amount\":\"1000000\",\"nativeTokens\":[],\"aliasId\":\"0xtestAliasID\","
-      "\"stateIndex\":12345,\"stateMetadata\":\"0xtestMetadataTestMetadataTestMetadata\",\"foundryCounter\":54321,"
-      "\"unlockConditions\":[{\"type\":4,\"address\":{\"type\":16,\"nftId\":"
-      "\"0x19c82b32761fd8729a1a6c77f7c17597e4b9b01759794e52381f6a0050b0c11f\"}}, "
+      "{\"type\":4,\"amount\":\"1000000\",\"nativeTokens\":[],\"aliasId\":"
+      "\"0xa2ea52db9c6185ef42e295dff8891c0d291c3fb7bbea8dadf1aaaa74adc6faad\",\"stateIndex\":12345,\"stateMetadata\":"
+      "\"0xtestMetadataTestMetadataTestMetadata\",\"foundryCounter\":54321,\"unlockConditions\":[{\"type\":4,"
+      "\"address\":{\"type\":16,\"nftId\":\"0x19c82b32761fd8729a1a6c77f7c17597e4b9b01759794e52381f6a0050b0c11f\"}}, "
       "{\"type\":5,\"address\":{\"type\":16,\"nftId\":"
       "\"0x19c82b32761fd8729a1a6c77f7c17597e4b9b01759794e52381f6a0050b0c11f\"}}],\"featureBlocks\":[],"
       "\"immutableFeatureBlocks\":[]}";
@@ -31,7 +31,10 @@ void test_parse_alias_output_basic() {
 
   TEST_ASSERT_EQUAL_UINT64(1000000, alias_output->amount);
   TEST_ASSERT_NULL(alias_output->native_tokens);
-  TEST_ASSERT_EQUAL_MEMORY("testAliasID", alias_output->alias_id, sizeof("testAliasID"));
+  byte_t alias_id[ALIAS_ID_BYTES];
+  hex_2_bin("a2ea52db9c6185ef42e295dff8891c0d291c3fb7bbea8dadf1aaaa74adc6faad", BIN_TO_HEX_BYTES(ALIAS_ID_BYTES), NULL,
+            alias_id, ALIAS_ID_BYTES);
+  TEST_ASSERT_EQUAL_MEMORY(alias_id, alias_output->alias_id, sizeof(alias_output->alias_id));
   TEST_ASSERT_EQUAL_UINT32(12345, alias_output->state_index);
   TEST_ASSERT_EQUAL_UINT32(37, alias_output->state_metadata->len);
   TEST_ASSERT_EQUAL_MEMORY("testMetadataTestMetadataTestMetadata", alias_output->state_metadata->data,
@@ -56,7 +59,7 @@ void test_parse_alias_output_full() {
       "\"0x08e781c2e4503f9e25207e21b2bddfd39995bdd0c40000000000000030000000000000000000\","
       "\"amount\":\"0x93847598347598347598347598\"},{\"id\":"
       "\"0x09e731c2e4503d9e25207e21b2bddfd39995bdd0c40000000000000000070000000000000000\",\"amount\":"
-      "\"0x7598347598347598\"}],\"aliasId\":\"0xtestAliasID\","
+      "\"0x7598347598347598\"}],\"aliasId\":\"0xa2ea52db9c6185ef42e295dff8891c0d291c3fb7bbea8dadf1aaaa74adc6faad\","
       "\"stateIndex\":12345,\"stateMetadata\":\"0xtestMetadataTestMetadataTestMetadata\",\"foundryCounter\":54321,"
       "\"unlockConditions\":[{\"type\":4,\"address\":{\"type\":16,\"nftId\":"
       "\"0x19c82b32761fd8729a1a6c77f7c17597e4b9b01759794e52381f6a0050b0c11f\"}}, "
@@ -89,7 +92,9 @@ void test_parse_alias_output_full() {
             BIN_TO_HEX_BYTES(NATIVE_TOKEN_ID_BYTES), NULL, token_id, NATIVE_TOKEN_ID_BYTES);
   TEST_ASSERT_NOT_NULL(native_tokens_find_by_id(alias_output->native_tokens, token_id));
 
-  TEST_ASSERT_EQUAL_MEMORY("testAliasID", alias_output->alias_id, sizeof("testAliasID"));
+  byte_t alias_id[ALIAS_ID_BYTES];
+  hex_2_bin("a2ea52db9c6185ef42e295dff8891c0d291c3fb7bbea8dadf1aaaa74adc6faad", BIN_TO_HEX_BYTES(ALIAS_ID_BYTES), NULL,
+            alias_id, ALIAS_ID_BYTES);
   TEST_ASSERT_EQUAL_UINT32(12345, alias_output->state_index);
   TEST_ASSERT_EQUAL_UINT32(37, alias_output->state_metadata->len);
   TEST_ASSERT_EQUAL_MEMORY("testMetadataTestMetadataTestMetadata", alias_output->state_metadata->data,
